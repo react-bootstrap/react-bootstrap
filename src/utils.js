a9ed8c12001f13e9b0c7bc2b2d3ef96f29a1d459
Pieter Vanderwerff <me@pieter.io>
2014-01-29 21:26:30 +1300

    Added new utils functions
    These functions are useful for working with Reacts `this.props.children`

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,67 @@ var ReactPropTransferer = require('react/lib/ReactPropTransferer');
 export default = {
 
   /**
+   * Modify each item in a React children array without
+   * unnecessarily allocating a new array.
+   *
+   * @param {array|object} children
+   * @param {function} modifier
+   * @returns {*}
+   */
+  modifyChildren: function (children, modifier) {
+    if (children == null) {
+      return children;
+    }
+
+    return Array.isArray(children) ? children.map(modifier) : modifier(children, 0);
+  },
+
+  /**
+   * Filter each item in a React children array without
+   * unnecessarily allocating a new array.
+   *
+   * @param {array|object} children
+   * @param {function} filter
+   * @returns {*}
+   */
+  filterChildren: function (children, filter) {
+    if (children == null) {
+      return children;
+    }
+
+    if (Array.isArray(children)) {
+      return children.filter(filter);
+    } else {
+      return filter(children, 0) ? children : null;
+    }
+  },
+
+
+  /**
+   * Safe chained function
+   *
+   * Will only create a new function if needed,
+   * otherwise will pass back existing functions or null.
+   *
+   * @param {function} one
+   * @param {function} two
+   * @returns {function|null}
+   */
+  createChainedFunction: function (one, two) {
+    var hasOne = typeof one === 'function';
+    var hasTwo = typeof two === 'function';
+
+    if (!hasOne && !hasTwo) { return null; }
+    if (!hasOne) { return two; }
+    if (!hasTwo) { return one; }
+
+    return function chainedFunction() {
+      one.apply(this, arguments);
+      two.apply(this, arguments);
+    };
+  },
+
+  /**
    * Sometimes you want to change the props of a child passed to you. Usually
    * this is to add a CSS class.
    *
