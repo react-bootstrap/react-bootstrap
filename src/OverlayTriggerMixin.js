1f821588ade60bcd50bf59730e6f542f343a7179
stevoland <stevoland@gmail.com>
2014-03-17 14:46:37 +0000

    Work around issue where `componentDidUpdate` is getting called in `OverlayTriggerMixin` before `componentDidMount`

--- src/OverlayTriggerMixin.js
+++ src/OverlayTriggerMixin.js
@@ -4,6 +4,7 @@ export default = {
   componentWillUnmount: function () {
     this._unrenderOverlay();
     document.body.removeChild(this._overlayTarget);
+    this._overlayTarget = null;
   },
 
   componentDidUpdate: function () {
@@ -11,16 +12,25 @@ export default = {
   },
 
   componentDidMount: function () {
-    this._overlayTarget = document.createElement('div');
-    document.body.appendChild(this._overlayTarget);
     this._renderOverlay();
   },
 
+  _mountOverlayTarget: function () {
+    this._overlayTarget = document.createElement('div');
+    document.body.appendChild(this._overlayTarget);
+  },
+
   _renderOverlay: function () {
-    React.renderComponent(this.renderOverlay(), this._overlayTarget);
+    if (!this._overlayTarget) {
+      this._mountOverlayTarget();
+    }
+
+    // Save reference to help testing
+    this._overlayInstance = React.renderComponent(this.renderOverlay(), this._overlayTarget);
   },
 
   _unrenderOverlay: function () {
     React.unmountComponentAtNode(this._overlayTarget);
+    this._overlayInstance = null;
   }
 };
