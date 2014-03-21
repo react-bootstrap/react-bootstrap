fbc3dc45750fd10170a0cb2dd6dfe26d4627bf8a
Pieter Vanderwerff <pieter@heyday.co.nz>
2014-03-21 21:18:16 +1300

    Reset whitespace to 2 spaces

--- src/OverlayTrigger.jsx
+++ src/OverlayTrigger.jsx
@@ -5,96 +5,96 @@ import cloneWithProps      from './react-es6/lib/cloneWithProps';
 import OverlayTriggerMixin from './OverlayTriggerMixin';
 
 var OverlayTrigger = React.createClass({
-    mixins: [OverlayTriggerMixin],
+  mixins: [OverlayTriggerMixin],
 
-    getDefaultProps: function () {
-        return {
-            trigger: 'click'
-        };
-    },
+  getDefaultProps: function () {
+    return {
+      trigger: 'click'
+    };
+  },
 
-    getInitialState: function() {
-        return {
-            isOverlayShown: (this.props.defaultOverlayShown == null) ?
-                false : this.props.defaultOverlayShown
-        };
-    },
+  getInitialState: function () {
+    return {
+      isOverlayShown: (this.props.defaultOverlayShown == null) ?
+        false : this.props.defaultOverlayShown
+    };
+  },
 
-    show: function () {
-        this.setState({
-            isOverlayShown: true
-        });
-    },
+  show: function () {
+    this.setState({
+      isOverlayShown: true
+    });
+  },
 
-    hide: function () {
-        this.setState({
-            isOverlayShown: false
-        });
-    },
+  hide: function () {
+    this.setState({
+      isOverlayShown: false
+    });
+  },
 
-    toggle: function () {
-        this.setState({
-            isOverlayShown: !this.state.isOverlayShown
-        });
-    },
+  toggle: function () {
+    this.setState({
+      isOverlayShown: !this.state.isOverlayShown
+    });
+  },
 
-    _trigger: function () {
-        var propName = 'delay' + (this.state.isOverlayShown ? 'Hide' : 'Show'),
-            delay = this.props[propName] || this.props.delay;
+  _trigger: function () {
+    var propName = 'delay' + (this.state.isOverlayShown ? 'Hide' : 'Show'),
+      delay = this.props[propName] || this.props.delay;
 
-        clearTimeout(this._triggerTimeout);
-        if (delay) {
-            this._triggerTimeout = setTimeout(this.toggle, parseInt(delay, 10));
-        } else {
-            this.toggle();
-        }
-    },
-
-    renderOverlay: function() {
-        var props;
-
-        if (!this.state.isOverlayShown || !this.props.overlay) {
-            return <span />;
-        }
-
-        props = {
-            onRequestHide: this._trigger
-        };
-
-        if (this.props.animation != null) {
-            props.animation = this.props.animation;
-        }
-
-        return cloneWithProps(
-            this.props.overlay,
-            props
-        );
-    },
-
-    render: function() {
-        return (this.props.children) ?
-            this.renderTrigger() : <span />;
-    },
-
-    renderTrigger: function () {
-        var props = {},
-            trigger = this.props.trigger,
-            propName;
-
-        if (trigger !== 'manual') {
-            if (trigger === 'hover') {
-                trigger = 'mouseOver';
-            }
-            propName = 'on' + trigger.substr(0, 1).toUpperCase() +
-                trigger.substr(1);
-            props[propName] = this._trigger;
-        }
-
-        return React.DOM.span(
-            props,
-            this.props.children
-        );
+    clearTimeout(this._triggerTimeout);
+    if (delay) {
+      this._triggerTimeout = setTimeout(this.toggle, parseInt(delay, 10));
+    } else {
+      this.toggle();
     }
+  },
+
+  renderOverlay: function () {
+    var props;
+
+    if (!this.state.isOverlayShown || !this.props.overlay) {
+      return <span />;
+    }
+
+    props = {
+      onRequestHide: this._trigger
+    };
+
+    if (this.props.animation != null) {
+      props.animation = this.props.animation;
+    }
+
+    return cloneWithProps(
+      this.props.overlay,
+      props
+    );
+  },
+
+  render: function () {
+    return (this.props.children) ?
+      this.renderTrigger() : <span />;
+  },
+
+  renderTrigger: function () {
+    var props = {},
+      trigger = this.props.trigger,
+      propName;
+
+    if (trigger !== 'manual') {
+      if (trigger === 'hover') {
+        trigger = 'mouseOver';
+      }
+      propName = 'on' + trigger.substr(0, 1).toUpperCase() +
+        trigger.substr(1);
+      props[propName] = this._trigger;
+    }
+
+    return React.DOM.span(
+      props,
+      this.props.children
+    );
+  }
 });
 
 export default = OverlayTrigger;
