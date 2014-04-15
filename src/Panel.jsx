12a685ae90c175bb869e117ab036c9fdef4703c3
Loic CHOLLIER <loic@chollier.com>
2014-04-15 16:29:21 -0700

    Only transfertProps to the Panel heading

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -128,7 +128,7 @@ var Panel = React.createClass({
     var classes = this.getBsClassSet();
     classes['panel'] = true;
 
-    return this.transferPropsTo(
+    return (
       <div className={classSet(classes)} id={this.props.isCollapsable ? null : this.props.id}>
         {this.renderHeading()}
         {this.props.isCollapsable ? this.renderCollapsableBody() : this.renderBody()}
