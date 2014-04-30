8dd379c83ab9628f0115c9e798038edd6a1a0e39
stevoland <stevoland@gmail.com>
2014-04-30 17:40:19 +0100

    Tweak `Grid`s example

--- docs/examples/GridBasic.js
+++ docs/examples/GridBasic.js
@@ -3,23 +3,23 @@
 var navInstance = (
     <Grid>
         <Row className="show-grid">
-          <Col xs={12} md={8}>.col-xs-12 .col-md-8</Col>
-          <Col xs={6} md={4}>.col-xs-6 .col-md-4</Col>
+          <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+          <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
         </Row>
 
         <Row className="show-grid">
-          <Col xs={6} md={4}>.col-xs-6 .col-md-4</Col>
-          <Col xs={6} md={4}>.col-xs-6 .col-md-4</Col>
-          <Col xs={6} md={4}>.col-xs-6 .col-md-4</Col>
+          <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+          <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+          <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
         </Row>
 
         <Row className="show-grid">
-          <Col xs={3} xsOffset={9}>.col-xs-3 .col-xs-offset-9</Col>
+          <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
         </Row>
 
         <Row className="show-grid">
-          <Col md={9} mdPush={3}>.col-md-9 .col-md-push-3</Col>
-          <Col md={3} mdPull={9}>.col-md-3 .col-md-pull-9</Col>
+          <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
+          <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
         </Row>
     </Grid>
   );
