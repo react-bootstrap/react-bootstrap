b3a6e79a0b4e14da5c58e8e0b3b54ac228309770
stevoland <stevoland@gmail.com>
2014-05-13 09:42:05 +0100

    Update React version in docs

--- docs/src/GettingStartedPage.js
+++ docs/src/GettingStartedPage.js
@@ -32,7 +32,7 @@ var Page = React.createClass({
                   <h3>CommonJS</h3>
                   <div className="highlight">
                     <pre><code className="shell">{'\
-  $ npm install react@v0.9.0\n\
+  $ npm install react@v0.10.0\n\
   $ npm install react-bootstrap\n\
                     '}</code></pre>
                     <pre><code className="js">{'\
@@ -48,7 +48,7 @@ var Page = React.createClass({
                   </div>
                   <div className="highlight">
                     <pre><code className="shell">{'\
-  $ bower install react#v0.9.0\n\
+  $ bower install react#v0.10.0\n\
   $ bower install git://github.com/react-bootstrap/react-bootstrap-bower.git\n\
                     '}</code></pre>
                     <pre><code className="js">{'\
@@ -62,7 +62,7 @@ var Page = React.createClass({
                   <p>The bower repo contains <code>react-bootstrap.js</code> and <code>react-botstrap.min.js</code> with all components exported in the <code>window.ReactBootstrap</code> object.</p>
                   <div className="highlight">
                     <pre><code className="html">{'\
-  <script src="http://fb.me/react-0.9.0.js"></script>\n\
+  <script src="http://fb.me/react-0.10.0.js"></script>\n\
   <script src="path/to/react-bootstrap-bower/react-bootstrap.min.js"></script>\n\
   <script>\n\
     var Alert = ReactBootstrap.Alert;\n\
