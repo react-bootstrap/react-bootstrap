0dc309085eaa10379ca8ba7f722ef856a0a4b882
stevo <stevoland@gmail.com>
2014-06-09 22:03:27 +0100

    Move source repo to organisation

--- docs/src/PageFooter.js
+++ docs/src/PageFooter.js
@@ -13,22 +13,22 @@ var PageHeader = React.createClass({
             <div className="bs-docs-social">
               <ul className="bs-docs-social-buttons">
                 <li>
-                  <iframe className="github-btn" src={'http://ghbtns.com/github-btn.html?user=stevoland&repo=react-bootstrap&type=watch&count=true'} width={90} height={20} title="Star on GitHub" />
+                  <iframe className="github-btn" src={'http://ghbtns.com/github-btn.html?user=react-bootstrap&repo=react-bootstrap&type=watch&count=true'} width={90} height={20} title="Star on GitHub" />
                 </li>
                 <li>
-                  <iframe className="github-btn" src={'http://ghbtns.com/github-btn.html?user=stevoland&repo=react-bootstrap&type=fork&count=true'} width={92} height={20} title="Fork on GitHub" />
+                  <iframe className="github-btn" src={'http://ghbtns.com/github-btn.html?user=react-bootstrap&repo=react-bootstrap&type=fork&count=true'} width={92} height={20} title="Fork on GitHub" />
                 </li>
               </ul>
             </div>
-            <p>Code licensed under <a href="https://github.com/stevoland/react-bootstrap/blob/master/LICENSE" target="_blank">MIT</a>.</p>
+            <p>Code licensed under <a href="https://github.com/react-bootstrap/react-bootstrap/blob/master/LICENSE" target="_blank">MIT</a>.</p>
             <ul className="bs-docs-footer-links muted">
               <li>Currently v{packageJSON.version}</li>
               <li>·</li>
-              <li><a href="https://github.com/stevoland/react-bootstrap/">GitHub</a></li>
+              <li><a href="https://github.com/react-bootstrap/react-bootstrap/">GitHub</a></li>
               <li>·</li>
-              <li><a href="https://github.com/stevoland/react-bootstrap/issues?state=open">Issues</a></li>
+              <li><a href="https://github.com/react-bootstrap/react-bootstrap/issues?state=open">Issues</a></li>
               <li>·</li>
-              <li><a href="https://github.com/stevoland/react-bootstrap/releases">Releases</a></li>
+              <li><a href="https://github.com/react-bootstrap/react-bootstrap/releases">Releases</a></li>
             </ul>
           </div>
         </footer>
--- tools/amd/README.md
+++ tools/amd/README.md
@@ -4,9 +4,9 @@
 
 This repo contains built AMD modules and standalone browser globals.
 
-There is a separate [source repo](https://github.com/stevoland/react-bootstrap).
+There is a separate [source repo](https://github.com/react-bootstrap/react-bootstrap).
 
 A [docs site](http://react-bootstrap.github.io) with live editable examples is a work in progress.
 
 
-[![Build Status](https://travis-ci.org/stevoland/react-bootstrap.png)](https://travis-ci.org/stevoland/react-bootstrap) [![Bower version](https://badge.fury.io/bo/react-bootstrap.png)](http://badge.fury.io/bo/react-bootstrap)
+[![Build Status](https://travis-ci.org/react-bootstrap/react-bootstrap.png)](https://travis-ci.org/react-bootstrap/react-bootstrap) [![Bower version](https://badge.fury.io/bo/react-bootstrap.png)](http://badge.fury.io/bo/react-bootstrap)
