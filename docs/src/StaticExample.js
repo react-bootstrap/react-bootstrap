8c48342607ba6592a9bec03bb274c5f728f6c8df
Matthew Honnibal <matthew@honnibal.com>
2014-07-10 14:58:38 +0200

    * Add StaticExample class, to display code snippets with syntax highlighting without the ReactPlayground editor. This needs review --- I didn't understand how the CodeMirror was used in ReactPlayground, so I copy-and-pasted.

---
+++ docs/src/StaticExample.js
@@ -0,0 +1,62 @@
+/** @jsx React.DOM */
+
+'use strict';
+
+var React = require('react');
+var CodeMirror = global.CodeMirror;
+
+var IS_MOBILE = typeof navigator !== 'undefined' && (
+  navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+  );
+
+
+var StaticExample = React.createClass({
+  componentDidMount: function() {
+    if (IS_MOBILE) return;
+
+    this.editor = CodeMirror.fromTextArea(this.refs.editor.getDOMNode(), {
+      mode: 'javascript',
+      lineNumbers: false,
+      lineWrapping: true,
+      matchBrackets: true,
+      tabSize: 2,
+      theme: 'solarized-light',
+      readOnly: 'nocursor'
+    });
+  },
+
+  componentDidUpdate: function() {
+    if (this.props.readOnly) {
+      this.editor.setValue(this.props.codeText);
+    }
+  },
+
+  render: function() {
+    // wrap in a div to fully contain CodeMirror
+    var editor;
+
+    if (IS_MOBILE) {
+      var preStyles = {overflow: 'scroll'};
+      editor = <pre style={preStyles}>{this.props.codeText}</pre>;
+    } else {
+      editor = <textarea ref="editor" defaultValue={this.props.codeText} />;
+    }
+
+    return (
+      <div style={this.props.style} className={this.props.className}>
+        {editor}
+        <br/>
+      </div>
+      );
+  }
+});
+
+
+module.exports = StaticExample;    
+
