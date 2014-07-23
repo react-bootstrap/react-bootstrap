6f445568d54a235110ba754001be1bc19a9ab05b
Matthew Honnibal <matthew@honnibal.com>
2014-07-23 13:09:02 +0100

    * Edits to introduction page, making it more complete and a bit tighter

--- docs/src/IntroductionPage.js
+++ docs/src/IntroductionPage.js
@@ -30,29 +30,48 @@ var Page = React.createClass({
             <div className="row">
               <div className="col-md-9" role="main">
                 <div className="bs-docs-section">
+                  <p className="lead">React-Bootstrap is a library of reuseable front-end components.  You'll get the look-and-feel of Twitter Bootstrap, but with much cleaner code, via Facebook's React.js framework.
+                  </p>
+                  <p>
+                    Let's say you want a small button that says "Something", to trigger the function someCallback. If were writing a native application, you might write something like:
+                  </p>
+                   {StaticExample({codeText: 'button(size=SMALL, color=GREEN, text="Something", onClick=someCallback)'})}
+                  <p>
+                    With the most popular web front-end framework, Twitter Bootstrap, you'd write this in your HTML:
+                  </p>
+                  {StaticExample({codeText: '<button id="something-btn" type="button" class="btn btn-success btn-sm">\n\tSomething\n</button>'})}
+                  <p>
+                    And something like <code>$('#something-btn').click(someCallback);</code> in your Javascript.
+                    By web standards this is quite nice, but it's still quite nasty.  React-Bootstrap lets you write this:
+                  </p>
+                    {StaticExample({codeText: '<Button bsStyle="success" bsSize="small" onClick={someCallback}>\n\tSomething\n</Button>'})}
 
                   <p>
-                    Let's say you want a small button that says "Something". Bootstrap lets you write this:
+                  The HTML/CSS implementation details are abstracted away, leaving you with an interface that more closely resembles what you would expect to write in other programming languages.
                   </p>
-                  {StaticExample({codeText: '<button id="something-btn" type="button" class="btn btn-success btn-sm">\n\tSome Text\n</button>'})}
-                  <p className="lead">
-                    Compared to other HTML/CSS solutions, this is quite nice; but compared to what's possible in any other language, it's quite nasty. With React-Bootstrap, you can write:
-                  </p>
-                    {StaticExample({codeText: '<Button bsStyle="success" bsSize="small" onClick={someHandler}>\n\tSomething\n</Button>'})}
                   <p>
-                    This is rendered by React into an element with the same markup as the Bootstrap example, and the click event is handled with React's virtual event system.
+                    Here's a more complicated example: a tabbed navigation area, showing the implementation with Bootstrap, and React-Bootstrap:
                   </p>
+
+                  <TabbedArea defaultActiveKey={2}>
+                    <TabPane key={1} tab="With Bootstrap">               
+                      <pre>{fs.readFileSync(__dirname + '/../comparisons/TabbedAreaBS.html', 'utf8')}</pre>
+                   </TabPane>
+                   <TabPane key={2} tab="With React-Bootstrap">
+                      <pre>{fs.readFileSync(__dirname + '/../comparisons/TabbedAreaRBS.jsx', 'utf8')}</pre>
+                   </TabPane>
+                  </TabbedArea>
 
                   <h2>A better Bootstrap API using React.js</h2>
                   <p>
-                    The Bootstrap code is so repetetive because HTML and CSS do not support the abstractions you need to write a nice library of components. That's why we have to write <small>btn</small> three times, within an element called <small>button</small>. 
+                    The Bootstrap code is so repetitive because HTML and CSS do not support the abstractions necessary for a nice library of components. That's why we have to write <small>btn</small> three times, within an element called <small>button</small>. 
                   </p>
           
                   <p><strong>The React.js solution is to write directly in Javascript.</strong>   React takes over the page-rendering entirely; you just give it a tree of Javascript objects, and tell it how state is transmitted between them.</p> 
 
                   <p>For instance, we might tell React to render a page displaying a single button, styled using the handy Bootstrap CSS:
                   </p>
-                  <ReactPlayground show={true} codeText={fs.readFileSync(__dirname + '/../comparisons/vanillaButton.js', 'utf8')} />
+                  <StaticExample codeText={fs.readFileSync(__dirname + '/../comparisons/vanillaButton.js', 'utf8')} />
                   <p>
                     But now that we're in Javascript, we can wrap the HTML/CSS, and provide a much better API:
                   </p>
@@ -63,20 +82,23 @@ var Page = React.createClass({
                   <p>
                     While each React component is really just a Javascript object, writing tree-structures that way gets tedious. React encourages the use of a syntactic-sugar called JSX, which lets you write the tree in an HTML-like syntax:
                   </p>
-                  <ReactPlayground show={true} codeText={fs.readFileSync(__dirname + '/../examples/ButtonGroupJustified.js', 'utf8')} />
-                  <h3>No More DOM Selectors</h3>
+                  <ReactPlayground show={true} codeText={fs.readFileSync(__dirname + '/../comparisons/ButtonToolbarDropdown.js', 'utf8')} />
+
                   <p>
-                    React-Bootstrap really shines on components that require some Javascript functionality.  In Bootstrap, this is achieved either through data attributes, or in Javascript, defined separately from the HTML code it references via arbitrary ID strings.
-                      For instance, here is a simple tabbed navigation area, displaying the difference in implementation:
+                   Some people's first impression of React.js is that it seems messy to mix Javascript and HTML in this way. 
+                     However, compare the code required to add
+
+                      a dropdown button in the example above to the <a href="http://getbootstrap.com/javascript/#dropdowns">
+                        Bootstrap Javascript</a> and <a href="http://getbootstrap.com/components/#btn-dropdowns">Components</a> documentation for creating a dropdown button. 
+                          The documentation is split in two because you have to implement the component in two places in your code: first you must add the HTML/CSS elements, and then you must call some Javascript setup code to wire the component together.
                   </p>
-                  <TabbedArea defaultActiveKey={2}>
-                    <TabPane key={1} tab="With Bootstrap">               
-                      <pre>{fs.readFileSync(__dirname + '/../comparisons/TabbedAreaBS.html', 'utf8')}</pre>
-                   </TabPane>
-                   <TabPane key={2} tab="With React-Bootstrap">
-                      <StaticExample codeText={fs.readFileSync(__dirname + '/../examples/TabbedAreaUncontrolled.js', 'utf8')} />
-                   </TabPane>
-                  </TabbedArea>
+                  <p>
+                  The React-Bootstrap component library tries to follow the React.js philosophy that a single piece of functionality should be defined in a single place.
+                  View the current React-Bootstrap library on the <a href="/components.html">components page</a>.
+                  </p>
+                  <p>
+                  The project is under active development --- APIs will change, and the documentation is far from complete. Contributions are encouraged!
+                  </p>
                 </div>
               </div>
             </div>
