a02c67d726cee419d5a6a02bc93c26e6175f60f9
Matthew Honnibal <matthew@honnibal.com>
2014-07-23 13:10:01 +0100

    * Add toolbar to home page, to give bigger links

--- docs/src/HomePage.js
+++ docs/src/HomePage.js
@@ -8,6 +8,9 @@ var NavMain = require('./NavMain');
 var PageHeader = require('./PageHeader');
 var PageFooter = require('./PageFooter');
 
+var Button = require('../../cjs/Button');
+var ButtonToolbar = require('../../cjs/ButtonToolbar');
+
 var HomePage = React.createClass({
   render: function () {
     return (
@@ -18,6 +21,11 @@ var HomePage = React.createClass({
             <div className="container">
               <span className="bs-docs-booticon bs-docs-booticon-lg bs-docs-booticon-outline"></span>
               <p className="lead">The most popular front-end framework, rebuilt for React.</p>
+              <ButtonToolbar bsSize="medium">
+                <Button bsStyle="primary" href="/getting-started.html">Get Started</Button>
+                <Button bsStyle="primary" href="http://github.com/react-bootstrap/">Github</Button>
+                <Button bsStyle="primary" href="/introduction.html">What is this?</Button>
+              </ButtonToolbar>
             </div>
           </main>
 
