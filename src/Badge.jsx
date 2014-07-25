c4aebcd5210ea336b633f58af9afba5ae7c5172e
Markus Wolf <mail@markus-wolf.de>
2014-07-25 15:27:05 +0200

    Added pullRight prop to Badge

--- src/Badge.jsx
+++ src/Badge.jsx
@@ -2,11 +2,26 @@
 
 var React = require('react');
 var ValidComponentChildren = require('./utils/ValidComponentChildren');
+var classSet = require('./utils/classSet');
 
 var Badge = React.createClass({
+  propTypes: {
+    pullRight: React.PropTypes.bool,
+  },
+
+  getDefaultProps: function () {
+    return {
+      pullRight: false
+    };
+  },
+
   render: function () {
+    var classes = {
+      'pull-right': this.props.pullRight,
+      'badge': ValidComponentChildren.hasValidComponent(this.props.children)
+    };
     return this.transferPropsTo(
-      <span className={ValidComponentChildren.hasValidComponent(this.props.children) ? 'badge': null}>
+      <span className={classSet(classes)}>
         {this.props.children}
       </span>
     );
