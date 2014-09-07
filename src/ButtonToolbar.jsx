d233f012bf1bcc417663dfde804eee00a1696022
Pieter Vanderwerff <me@pieter.io>
2014-09-07 21:13:16 +1200

    Fixed `ButtonToolbar` displayName
    Closes #220

--- src/ButtonToolbar.jsx
+++ src/ButtonToolbar.jsx
@@ -5,7 +5,7 @@ var classSet = require('./utils/classSet');
 var BootstrapMixin = require('./BootstrapMixin');
 var Button = require('./Button');
 
-var ButtonGroup = React.createClass({
+var ButtonToolbar = React.createClass({
   mixins: [BootstrapMixin],
 
   getDefaultProps: function () {
@@ -27,4 +27,4 @@ var ButtonGroup = React.createClass({
   }
 });
 
-module.exports = ButtonGroup;
+module.exports = ButtonToolbar;
