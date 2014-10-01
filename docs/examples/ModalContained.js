7f11590b18a5b62fe66e055bd29c413cc67ff4a4
Matthew Smith <mtscout6@gmail.com>
2014-10-01 14:27:54 -0600

    Added contained modal example

---
+++ docs/examples/ModalContained.js
@@ -0,0 +1,42 @@
+/** @jsx React.DOM */
+
+/**
+ * You will want to include this bit of css
+ *
+ * .modal-container {
+ *   position: relative;
+ * }
+ * .modal-container .modal, .modal-container .modal-backdrop {
+ *   position: absolute;
+ * }
+ */
+
+
+var ContainedModal = React.createClass({
+  render: function() {
+    return this.transferPropsTo(
+      <Modal title='Contained Modal' animation>
+        <div className="modal-body">
+          Elit est explicabo ipsum eaque dolorem blanditiis doloribus sed id ipsam, beatae, rem fuga id earum? Inventore et facilis obcaecati.
+        </div>
+        <div className="modal-footer">
+          <Button onClick={this.props.onRequestHide}>Close</Button>
+        </div>
+      </Modal>
+    );
+  }
+});
+
+var Trigger = React.createClass({
+  render: function() {
+    return (
+      <div className='modal-container' style={{height: 200}}>
+        <ModalTrigger modal={<ContainedModal container={this} />} container={this}>
+          <Button bsStyle="primary" bsSize="large">Launch contained modal</Button>
+        </ModalTrigger>
+      </div>
+    );
+  }
+});
+
+React.renderComponent(<Trigger />, mountNode);
