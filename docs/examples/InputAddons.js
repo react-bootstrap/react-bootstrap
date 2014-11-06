8d8c0b4a20afdbc04c57301e3f055235246ace18
Matthew Lyon <matthew@lyonheart.us>
2014-11-06 14:50:08 -0800

    docs for before/afterButton

--- docs/examples/InputAddons.js
+++ docs/examples/InputAddons.js
@@ -4,6 +4,10 @@ var inputAddonsInstance = (
       <Input type="text" addonAfter=".00" />
       <Input type="text" addonBefore="$" addonAfter=".00" />
       <Input type="text" addonAfter={<Glyphicon glyph="music" />} />
+      <Input type="text" buttonBefore={<Button>Before</Button>} />
+      <Input type="text" buttonAfter={<DropdownButton title="Action">
+        <MenuItem key="1">Item</MenuItem>
+      </DropdownButton>} />
     </form>
   );
 
