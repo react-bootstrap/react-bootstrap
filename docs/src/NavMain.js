d0aaefa76d2bfd427e59d744f9a5b0aab9957e65
stevo <stevoland@gmail.com>
2014-11-19 21:52:59 +0000

    Fix mobile navbar in docs

--- docs/src/NavMain.js
+++ docs/src/NavMain.js
@@ -28,7 +28,7 @@ var NavMain = React.createClass({
 
     return (
       <Navbar componentClass='header' brand={brand} staticTop className="bs-docs-nav" role="banner" toggleNavKey={0}>
-        <Nav className="bs-navbar-collapse" role="navigation" key={0} id="top">
+        <Nav className="bs-navbar-collapse" role="navigation" eventKey={0} id="top">
           {Object.keys(NAV_LINKS).map(this.renderNavItem)}
         </Nav>
       </Navbar>
