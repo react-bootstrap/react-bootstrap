37f0fe06db1c508bb91f322e619b74c56e6fb484
AlexKVal <alexkval@gmail.com>
2015-04-13 17:41:26 +0300

    [fix] Enable `development` mode for tests.
    Test is included.
    
    When mode is `production` webpack strips out code for development mode.
    React `console warnings` also are removed in `production`.
    
    In that case tests don't `see` any console warnings from React.
    Consequently code like this (in tests)
      console.warn.called.should.be.false;
    is useless.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,10 @@
 import from 'es5-shim';
+
+describe('Process environment for tests', function () {
+  it('Should be development for React console warnings', function () {
+    assert.equal(process.env.NODE_ENV, 'development');
+  });
+});
+
 const testsContext = require.context('.', true, /Spec$/);
 testsContext.keys().forEach(testsContext);
