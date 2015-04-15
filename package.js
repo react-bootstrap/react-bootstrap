8a5e61944059527287cc53ec31f1471cb7cf7134
Igor Loskutov <igor.loskutoff@gmail.com>
2015-04-15 16:14:09 +0700

    Meteor build

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'firfi:meteor-react-bootstrap',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'React-bootstrap port for Meteor',
   // URL to the Git repository containing the source code for this package.
