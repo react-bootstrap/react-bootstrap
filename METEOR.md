d5da88367e53a5ab25f4265f07cb588ac1e58d38
Igor Loskutov <igor.loskutoff@gmail.com>
2015-04-15 16:10:40 +0700

    Meteor build

---
+++ METEOR.md
@@ -0,0 +1,3 @@
+[React-bootstrap](https://github.com/react-bootstrap/react-bootstrap) bundle for Meteor. Exports ReactBootstrap global var.
+
+When and if bundle manually, don't forget special Meteor Hack to make it work.
---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'firfi:meteor-react-bootstrap',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'React-bootstrap port for Meteor',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/Firfi/meteor-react-bootstrap',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'METEOR.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.5');
+  api.use('reactjs:react@0.2.1');
+  api.use('twbs:bootstrap@3.3.4', 'client');
+  api.addFiles('dist/react-bootstrap.js');
+  api.export(['ReactBootstrap']); 
+});
+
+Package.onTest(function(api) {
+
+});
