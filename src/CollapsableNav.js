2b8dc5fb3ea46de84a7f56c91831c85cd9232aa2
AlexKVal <alexkval@gmail.com>
2015-05-04 23:06:02 +0300

    Fix the subtle bug with deprecation warning.
    When deprecated `Collaps_a_bleNav` is imported
    it also tags `Collaps_i_bleNav` element as deprecated, because
    it is done by using pointers which points to the same object.
    
    ```js
    let CollapsableNav = CollapsibleNav;
    CollapsableNav.__deprecated__ = true;
    ```
    https://github.com/react-bootstrap/react-bootstrap/blob/92c57ef7ee/src/CollapsableNav.js#L5
    
    The right and simplest way to make deprecation
    in the case of component renaming `CollapsableNav => CollapsibleNav`
    by using general for both components part - `classSpecifications`.
    
    Like this:
    
    ```js
    // NewNameComponent.js
    const classSpecificationsFor_NewNameComponent = { /* existing code */ };
    const NewNameComponent =
      React.createClass( classSpecificationsFor_NewNameComponent );
    export {classSpecificationsFor_NewNameComponent}; // for old component
    export default NewNameComponent;
    
    // OldNameComponent.js
    import {classSpecificationsFor_NewNameComponent} from 'NewNameComponent';
    const classSpecsFor_OldNameComponent =
      assign({}, classSpecificationsFor_NewNameComponent, {
      // here we add deprecation warning
      });
    const OldNameComponent =
      React.createClass( classSpecsFor_OldNameComponent );
    export default OldNameComponent;
    ```

--- src/CollapsableNav.js
+++ src/CollapsableNav.js
@@ -1,7 +1,18 @@
-import CollapsibleNav from './CollapsibleNav';
+import React from 'react';
+import deprecationWarning from './utils/deprecationWarning';
+import assign from './utils/Object.assign';
+import {specCollapsibleNav} from './CollapsibleNav';
 
-let CollapsableNav = CollapsibleNav;
+const specCollapsableNav = assign({}, specCollapsibleNav, {
+  componentDidMount() {
+    deprecationWarning(
+      'CollapsableNav',
+      'CollapsibleNav',
+      'https://github.com/react-bootstrap/react-bootstrap/issues/425#issuecomment-97110963'
+    );
+  }
+});
 
-CollapsableNav.__deprecated__ = true;
+const CollapsableNav = React.createClass(specCollapsableNav);
 
 export default CollapsableNav;
