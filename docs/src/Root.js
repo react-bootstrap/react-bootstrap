1d02171b5ae982c3f7ffb0ae4a40f4ee07281c68
Matthew Smith <mtscout6@gmail.com>
2015-05-17 10:06:34 -0600

    Fix single quote bugs for docs in production
    Part of #677

--- docs/src/Root.js
+++ docs/src/Root.js
@@ -46,16 +46,16 @@ const Root = React.createClass({
 
     let head = {
       __html: `<title>React Bootstrap</title>
-        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <link href='${this.props.assetBaseUrl}/assets/bundle.css' rel='stylesheet' />
-        <link href='${this.props.assetBaseUrl}/assets/favicon.ico' type='image/x-icon' rel='icon' />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="${this.props.assetBaseUrl}/assets/bundle.css" rel="stylesheet">
+        <link href="${this.props.assetBaseUrl}/assets/favicon.ico" type="image/x-icon" rel="icon">
         <!--[if lt IE 9]>
-        <script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>
-        <script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>
-        <script src='http://code.jquery.com/jquery-1.11.1.min.js'></script>
-        <script src='http://cdnjs.cloudflare.com/ajax/libs/es5-shim/3.4.0/es5-shim.js'></script>
-        <script src='http://cdnjs.cloudflare.com/ajax/libs/es5-shim/3.4.0/es5-sham.js'></script>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/es5-shim/3.4.0/es5-shim.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/es5-shim/3.4.0/es5-sham.js"></script>
         <![endif]-->`
     };
 
