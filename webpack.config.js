2a1d7f6b8d365441d27427b6f1ea777bbadb22e5
AlexKVal <alexkval@gmail.com>
2015-05-17 20:57:40 +0300

    Simplify webpack config files.
    Remove some stale `IE8` pieces.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 /* eslint no-var: 0 */
 require('babel/register');
 var config = require('./webpack/webpack.config');
-var result = config();
-module.exports = result;
+module.exports = config;
---
+++ webpack/base.config.js
@@ -0,0 +1,41 @@
+import webpack from 'webpack';
+import yargs from 'yargs';
+
+export const options = yargs
+  .alias('p', 'optimize-minimize')
+  .alias('d', 'debug')
+  .option('port', {
+    default: '8080',
+    type: 'string'
+  })
+  .argv;
+
+export const jsLoader = 'babel';
+
+const baseConfig = {
+  entry: undefined,
+
+  output: undefined,
+
+  externals: undefined,
+
+  module: {
+    loaders: [
+      { test: /\.js/, loader: jsLoader, exclude: /node_modules/ }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(options.optimizeMinimize ? 'production' : 'development')
+      }
+    })
+  ]
+};
+
+if (options.optimizeMinimize) {
+  baseConfig.devtool = 'source-map';
+}
+
+export default baseConfig;
