aebf924c43e525727d9a6b81e7cae2198e1caa12
Thomas <iammotivated@gmail.com>
2015-05-27 23:05:30 +0100

    Removed createContextWrapper from export utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 import childrenValueInputValidation from './childrenValueInputValidation';
 import createChainedFunction from './createChainedFunction';
-import createContextWrapper from './createContextWrapper';
 import CustomPropTypes from './CustomPropTypes';
 import domUtils from './domUtils';
 import ValidComponentChildren from './ValidComponentChildren';
@@ -8,7 +7,6 @@ import ValidComponentChildren from './ValidComponentChildren';
 export default {
   childrenValueInputValidation,
   createChainedFunction,
-  createContextWrapper,
   CustomPropTypes,
   domUtils,
   ValidComponentChildren
