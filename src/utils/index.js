8c66b4ed3899ec56b75deec7ad237bdbb6db02c4
Thomas <iammotivated@gmail.com>
2015-05-27 22:17:04 +0100

    removed deprecationWarning and assign

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,8 @@ import childrenValueInputValidation from './childrenValueInputValidation';
 import createChainedFunction from './createChainedFunction';
 import createContextWrapper from './createContextWrapper';
 import CustomPropTypes from './CustomPropTypes';
-import deprecationWarning from './deprecationWarning';
 import domUtils from './domUtils';
 import EventListener from './EventListener';
-import assign from './Object.assign';
 import ValidComponentChildren from './ValidComponentChildren';
 
 export default {
@@ -13,9 +11,7 @@ export default {
   createChainedFunction,
   createContextWrapper,
   CustomPropTypes,
-  deprecationWarning,
   domUtils,
   EventListener,
-  assign,
   ValidComponentChildren
 };
