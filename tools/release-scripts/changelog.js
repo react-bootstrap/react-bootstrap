53d9b43f1f04a233e17e77cd048f042b5e3f7321
Matthew Smith <mtscout6@gmail.com>
2015-06-02 10:56:06 -0600

    Fix alpha changelog removal if it doesn't exist

--- tools/release-scripts/changelog.js
+++ tools/release-scripts/changelog.js
@@ -1,4 +1,5 @@
 import 'colors';
+import fsp from 'fs-promise';
 import { exec, safeExec } from '../exec';
 import { changelog, alphaChangelog } from '../constants';
 import semver from 'semver';
@@ -9,14 +10,30 @@ export default (version) => {
   let result = Promise.resolve();
   let output = isPrerelease ? alphaChangelog : changelog;
   let additionalArgs = '';
+  let removedAlphaChangelog = false;
 
   if (!isPrerelease) {
-    result = exec(`rimraf ${alphaChangelog}`);
+    result = fsp.exists(alphaChangelog)
+      .then(exists => {
+        if (!exists) {
+          return null;
+        }
+
+        removedAlphaChangelog = true;
+        return exec(`rimraf ${alphaChangelog}`);
+      });
     additionalArgs = ' --exclude-pre-releases';
   }
 
   return result
     .then(() => exec(`node_modules/.bin/changelog --title v${version} --out ${output}${additionalArgs}`))
-    .then(() => safeExec(`git add -A ${changelog} ${alphaChangelog}`))
+    .then(() => safeExec(`git add ${changelog}`))
+    .then(() => {
+      if (!removedAlphaChangelog) {
+        return null;
+      }
+
+      return safeExec(`git add -A ${alphaChangelog}`);
+    })
     .then(() => console.log('Generated Changelog'.cyan));
 };
