8d31afe6dbb787c9bae286b3745d254b438b4cff
Matthew Smith <mtscout6@gmail.com>
2015-06-17 08:31:03 -0600

    Add tooling for test code coverage analysis.

--- webpack/test.config.js
+++ webpack/test.config.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
-import baseConfig from './base.config';
+import baseConfig, { jsLoader } from './base.config';
 
 export default _.extend({}, baseConfig, {
   output: {
     pathinfo: true
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js/, loader: `${jsLoader}!isparta`, exclude: /node_modules|test[\/\\]/ },
+      { test: /\.js/, loader: `${jsLoader}`, include: /test[\/\\]/ }
+    ]
   },
 
   devtool: 'eval'
