8a9f415c08b6a82bfe31e4b80d6b0ded7551a251
jquense <monastic.panic@gmail.com>
2015-06-20 22:34:30 -0400

    Add eslint babel plugin

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-/* eslint no-var: 0, object-shorthand: 0 */
+/* eslint no-var: 0, babel/object-shorthand: 0 */
 require('babel/register');
 
 var webpackConfig = require('./webpack/test.config.js');
