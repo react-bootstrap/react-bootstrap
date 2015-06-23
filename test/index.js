64f77313e75d0bb1cce38a7c9e9f7f5f840865f2
Matthew Smith <mtscout6@gmail.com>
2015-06-22 19:57:53 -0600

    Ensure all src folder js files are loaded for full code coverage analysis

--- test/index.js
+++ test/index.js
@@ -19,5 +19,9 @@ describe('Process environment for tests', function () {
   });
 });
 
+// Ensure all files in src folder are loaded for proper code coverage analysis
+const srcContext = require.context('../src', true, /.*\.js$/);
+srcContext.keys().forEach(srcContext);
+
 const testsContext = require.context('.', true, /Spec$/);
 testsContext.keys().forEach(testsContext);
