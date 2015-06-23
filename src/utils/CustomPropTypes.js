0e5980f890786f3c956d6fe91177edd9e02dc040
AlexKVal <alexkval@gmail.com>
2015-06-23 18:48:07 +0300

    [added] 'elementType' custom prop type validator
    The code has been borrowed from
    the https://github.com/jquense/react-widgets project

--- src/utils/CustomPropTypes.js
+++ src/utils/CustomPropTypes.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const ANONYMOUS = '<<anonymous>>';
 
 const CustomPropTypes = {
@@ -14,6 +16,20 @@ const CustomPropTypes = {
    * @returns {Error|undefined}
    */
   mountable: createMountableChecker(),
+
+  /**
+   * Checks whether a prop provides a type of element.
+   *
+   * The type of element can be provided in two forms:
+   * - tag name (string)
+   * - a return value of React.createClass(...)
+   *
+   * @param props
+   * @param propName
+   * @param componentName
+   * @returns {Error|undefined}
+   */
+  elementType: createElementTypeChecker(),
 
   /**
    * Checks whether a prop matches a key of an associated object
@@ -133,6 +149,26 @@ function all(propTypes) {
       }
     }
   };
+}
+
+function createElementTypeChecker() {
+  function validate(props, propName, componentName) {
+    let errMsg = `Invalid prop '${propName}' specified in '${componentName}'.` +
+      ' Expected an Element `type`';
+
+    if (typeof props[propName] !== 'function') {
+      if (React.isValidElement(props[propName])) {
+        return new Error(errMsg + ', not an actual Element');
+      }
+
+      if (typeof props[propName] !== 'string') {
+        return new Error(errMsg +
+          ' such as a tag name or return value of React.createClass(...)');
+      }
+    }
+  }
+
+  return createChainableTypeChecker(validate);
 }
 
 export default CustomPropTypes;
