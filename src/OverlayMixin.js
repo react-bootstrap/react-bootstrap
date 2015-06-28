f79911036460aaac8f3c8d76fd12c6cb91372c60
jquense <monastic.panic@gmail.com>
2015-06-28 17:44:18 -0400

    [added] Portal component; replaces OverlayMixin

--- src/OverlayMixin.js
+++ src/OverlayMixin.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import CustomPropTypes from './utils/CustomPropTypes';
 import domUtils from './utils/domUtils';
+import deprecationWarning from './utils/deprecationWarning';
 
-export default {
+export const OverlayMixin = {
   propTypes: {
+
     container: CustomPropTypes.mountable
   },
 
-  componentWillUnmount() {
-    this._unrenderOverlay();
-    if (this._overlayTarget) {
-      this.getContainerDOMNode()
-        .removeChild(this._overlayTarget);
-      this._overlayTarget = null;
-    }
+
+  componentDidMount() {
+    this._renderOverlay();
   },
 
   componentDidUpdate() {
     this._renderOverlay();
   },
 
-  componentDidMount() {
-    this._renderOverlay();
+  componentWillUnmount() {
+    this._unrenderOverlay();
+    this._mountOverlayTarget();
   },
 
   _mountOverlayTarget() {
-    this._overlayTarget = document.createElement('div');
-    this.getContainerDOMNode()
-      .appendChild(this._overlayTarget);
+    if (!this._overlayTarget) {
+      this._overlayTarget = document.createElement('div');
+      this.getContainerDOMNode()
+        .appendChild(this._overlayTarget);
+    }
+  },
+
+  _unmountOverlayTarget() {
+    if (this._overlayTarget) {
+      this.getContainerDOMNode()
+        .removeChild(this._overlayTarget);
+      this._overlayTarget = null;
+    }
   },
 
   _renderOverlay() {
@@ -39,10 +48,12 @@ export default {
 
     // Save reference to help testing
     if (overlay !== null) {
+      this._mountOverlayTarget();
       this._overlayInstance = React.render(overlay, this._overlayTarget);
     } else {
       // Unrender if the component is null for transitions to null
       this._unrenderOverlay();
+      this._unmountOverlayTarget();
     }
   },
 
@@ -65,5 +76,16 @@ export default {
 
   getContainerDOMNode() {
     return React.findDOMNode(this.props.container) || domUtils.ownerDocument(this).body;
+  }
+};
+
+export default {
+
+  ...OverlayMixin,
+
+  componentWillMount() {
+    deprecationWarning(
+        'Overlay mixin', 'the `<Portal/>` Component'
+      , 'http://react-bootstrap.github.io/components.html#modals-custom');
   }
 };
