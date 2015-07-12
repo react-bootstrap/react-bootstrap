5f2de26cb56f31f13c11c017647281708b165800
Patrick Smith <pgwsmith@gmail.com>
2015-07-12 22:29:37 +1000

    Allow ModalHeader’s closeButton to take a ReactElement

--- src/ModalHeader.js
+++ src/ModalHeader.js
@@ -4,24 +4,35 @@ import classnames from 'classnames';
 
 class ModalHeader extends React.Component {
 
+  renderCloseButton() {
+    if (this.props.closeButton === true) {
+      return (
+        <button
+          className='close'
+          aria-label={this.props['aria-label'] || 'Close'} //eslint-disable-line react/prop-types
+          onClick={this.props.onHide}
+          style={{ marginTop: -2 }}
+        >
+          <span aria-hidden="true">
+            &times;
+          </span>
+        </button>
+      );
+    }
+    else if (React.isValidElement(this.props.closeButton)) {
+      return React.cloneElement(this.props.closeButton, {
+        onClick: this.props.onHide
+      });
+    }
+  }
+
   render() {
     return (
       <div
         {...this.props}
         className={classnames(this.props.className, this.props.modalClassName)}
       >
-        { this.props.closeButton &&
-          <button
-            className='close'
-            aria-label={this.props['aria-label'] || 'Close'} //eslint-disable-line react/prop-types
-            onClick={this.props.onHide}
-            style={{ marginTop: -2 }}
-          >
-            <span aria-hidden="true">
-              &times;
-            </span>
-          </button>
-        }
+        { this.renderCloseButton() }
         { this.props.children }
       </div>
     );
@@ -39,7 +50,10 @@ ModalHeader.propTypes = {
   /**
    * Specify whether the Component should contain a close button
    */
-  closeButton: React.PropTypes.bool,
+  closeButton: React.PropTypes.oneOfType([
+    React.PropTypes.bool,
+    React.PropTypes.element
+  ]),
   /**
    * A Callback fired when the close button is clicked. If used directly inside a Modal component, the onHide will automatically
    * be propagated up to the parent Modal `onHide`.
