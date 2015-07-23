0944cddda8c5707e454a2afb054f8b20f102380d
AlexKVal <alexkval@gmail.com>
2015-07-23 13:07:24 +0300

    Add standalone collapsible Panel example

---
+++ docs/examples/PanelCollapsible.js
@@ -0,0 +1,24 @@
+class Example extends React.Component {
+  constructor(...args){
+    super(...args);
+    this.state = {
+      open: true
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Button onClick={ ()=> this.setState({ open: !this.state.open })}>
+          click
+        </Button>
+        <Panel collapsible expanded={this.state.open}>
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+          Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+        </Panel>
+      </div>
+    );
+  }
+}
+
+React.render(<Example/>, mountNode);
