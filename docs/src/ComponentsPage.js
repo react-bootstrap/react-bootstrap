674d67e91ad0321988f246eef9498b88c520b33a
Budi Irawan <deerawan@gmail.com>
2015-09-05 15:56:27 +0700

    [added] images component
    image testing done
    
    add docs
    
    add comment for props
    
    * Merge previous three samples (ImageCircle, ImageRounded, ImageThumbnail) to one sample file (ImageShape)
    
    * Add ImageResponsive and ImageShape in ReactPlayground.js
    
    * Add Image source include in Samples.js
    
    fix the comment
    
    * Fix eslint issue on Image.js
    * Fix eslint issue on ImageSpec.js
    
    * remove src and alt
    * refactor test code to test attribute not props
    
    fix eslint

--- docs/src/ComponentsPage.js
+++ docs/src/ComponentsPage.js
@@ -904,6 +904,22 @@ const ComponentsPage = React.createClass({
                   <h4><Anchor id="utilities-fade-props">Props</Anchor></h4>
                   <PropTable component="Fade"/>
                 </div>
+
+                {/* Images */}
+                <div className="bs-docs-section">
+                  <h1 className="page-header"><Anchor id="images">Images</Anchor></h1>
+
+                  <h3><Anchor id="image-shape">Shape</Anchor></h3>
+                  <p>Use the <code>rounded</code>, <code>circle</code> and <code>thumbnail</code> props to customise the image.</p>
+                  <ReactPlayground codeText={Samples.ImageShape} />
+
+                  <h3><Anchor id="image-responsive">Responsive</Anchor></h3>
+                  <p>Use the <code>responsive</code> to scale image nicely to the parent element.</p>
+                  <ReactPlayground codeText={Samples.ImageResponsive} />
+
+                  <h3><Anchor id="image-props">Props</Anchor></h3>
+                  <PropTable component="Image"/>
+                </div>
               </div>
 
 
@@ -948,6 +964,7 @@ const ComponentsPage = React.createClass({
                     <NavItem href="#tables" key={25}>Tables</NavItem>
                     <NavItem href="#input" key={26}>Input</NavItem>
                     <NavItem href="#utilities" key={28}>Utilities</NavItem>
+                    <NavItem href="#images" key={29}>Images</NavItem>
                   </Nav>
                   <a className="back-to-top" href="#top">
                   Back to top
