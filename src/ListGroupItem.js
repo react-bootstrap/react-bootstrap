b7d33291a74b8fb1e8a96ebb0fca812bb18684c5
Jimmy Jia <tesrin@gmail.com>
2015-09-15 00:55:48 -0400

    Removed unused props from ListGroupItem

--- src/ListGroupItem.js
+++ src/ListGroupItem.js
@@ -13,9 +13,7 @@ const ListGroupItem = React.createClass({
     header: React.PropTypes.node,
     listItem: React.PropTypes.bool,
     onClick: React.PropTypes.func,
-    eventKey: React.PropTypes.any,
-    href: React.PropTypes.string,
-    target: React.PropTypes.string
+    href: React.PropTypes.string
   },
 
   getDefaultProps() {
