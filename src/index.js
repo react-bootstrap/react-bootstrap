ca52c30bdb7636074c29071ee42e3d17bd066181
Jimmy Jia <tesrin@gmail.com>
2015-10-07 09:46:46 -0400

    [fixed] Actually export the Image component

--- src/index.js
+++ src/index.js
@@ -4,25 +4,21 @@ export AffixMixin from './AffixMixin';
 export Alert from './Alert';
 export Badge from './Badge';
 export BootstrapMixin from './BootstrapMixin';
+export Breadcrumb from './Breadcrumb';
+export BreadcrumbItem from './BreadcrumbItem';
 export Button from './Button';
 export ButtonGroup from './ButtonGroup';
 export ButtonInput from './ButtonInput';
 export ButtonToolbar from './ButtonToolbar';
-export Breadcrumb from './Breadcrumb';
-export BreadcrumbItem from './BreadcrumbItem';
 export Carousel from './Carousel';
 export CarouselItem from './CarouselItem';
 export Col from './Col';
-
 export CollapsibleNav from './CollapsibleNav';
-
 export Dropdown from './Dropdown';
 export DropdownButton from './DropdownButton';
-export NavDropdown from './NavDropdown';
-export SplitButton from './SplitButton';
-
 export Glyphicon from './Glyphicon';
 export Grid from './Grid';
+export Image from './Image';
 export Input from './Input';
 export Interpolate from './Interpolate';
 export Jumbotron from './Jumbotron';
@@ -30,21 +26,18 @@ export Label from './Label';
 export ListGroup from './ListGroup';
 export ListGroupItem from './ListGroupItem';
 export MenuItem from './MenuItem';
-
 export Modal from './Modal';
-export ModalHeader from './ModalHeader';
-export ModalTitle from './ModalTitle';
 export ModalBody from './ModalBody';
 export ModalFooter from './ModalFooter';
-
+export ModalHeader from './ModalHeader';
+export ModalTitle from './ModalTitle';
 export Nav from './Nav';
-export NavBrand from './NavBrand';
 export Navbar from './Navbar';
+export NavBrand from './NavBrand';
+export NavDropdown from './NavDropdown';
 export NavItem from './NavItem';
-
 export Overlay from './Overlay';
 export OverlayTrigger from './OverlayTrigger';
-
 export PageHeader from './PageHeader';
 export PageItem from './PageItem';
 export Pager from './Pager';
@@ -57,13 +50,12 @@ export ResponsiveEmbed from './ResponsiveEmbed';
 export Row from './Row';
 export SafeAnchor from './SafeAnchor';
 export SplitButton from './SplitButton';
+export SplitButton from './SplitButton';
 export styleMaps from './styleMaps';
 export SubNav from './SubNav';
-export Table from './Table';
-
 export Tab from './Tab';
+export Table from './Table';
 export Tabs from './Tabs';
-
 export Thumbnail from './Thumbnail';
 export Tooltip from './Tooltip';
 export Well from './Well';
