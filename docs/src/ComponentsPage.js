83d5be9310d32990ba6c7ffc3870e52ce1d6b621
Jimmy Jia <tesrin@gmail.com>
2015-11-15 13:10:32 -0500

    Components page NavItems don't need keys

--- docs/src/ComponentsPage.js
+++ docs/src/ComponentsPage.js
@@ -1033,42 +1033,42 @@ const ComponentsPage = React.createClass({
                       activeHref={this.state.activeNavItemHref}
                       onSelect={this.handleNavItemSelect}
                     >
-                      <SubNav href="#buttons" key={1} text="Buttons">
-                        <NavItem href="#btn-groups" key={2}>Button groups</NavItem>
-                        <NavItem href="#btn-dropdowns" key={3}>Dropdowns</NavItem>
-                        <NavItem href="#menu-items" key={25}>Menu items</NavItem>
+                      <SubNav href="#buttons" text="Buttons">
+                        <NavItem href="#btn-groups">Button groups</NavItem>
+                        <NavItem href="#btn-dropdowns">Dropdowns</NavItem>
+                        <NavItem href="#menu-items">Menu items</NavItem>
                       </SubNav>
-                      <NavItem href="#panels" key={4}>Panels</NavItem>
-                      <NavItem href="#modals" key={5}>Modals</NavItem>
-                      <NavItem href="#tooltips" key={6}>Tooltips</NavItem>
-                      <NavItem href="#popovers" key={7}>Popovers</NavItem>
-                      <NavItem href="#overlays" key={27}>Overlays</NavItem>
-                      <NavItem href="#progress" key={8}>Progress bars</NavItem>
-                      <NavItem href="#navs" key={9}>Navs</NavItem>
-                      <NavItem href="#navbars" key={10}>Navbars</NavItem>
-                      <NavItem href="#breadcrumbs" key={30}>Breadcrumbs</NavItem>
-                      <NavItem href="#tabs" key={11}>Tabs</NavItem>
-                      <NavItem href="#pager" key={12}>Pager</NavItem>
-                      <NavItem href="#pagination" key={13}>Pagination</NavItem>
-                      <NavItem href="#alerts" key={14}>Alerts</NavItem>
-                      <NavItem href="#carousels" key={15}>Carousels</NavItem>
-                      <NavItem href="#grids" key={16}>Grids</NavItem>
-                      <NavItem href="#images" key={29}>Images</NavItem>
-                      <NavItem href="#thumbnail" key={17}>Thumbnail</NavItem>
-                      <NavItem href="#listgroup" key={18}>List group</NavItem>
-                      <NavItem href="#labels" key={19}>Labels</NavItem>
-                      <NavItem href="#badges" key={20}>Badges</NavItem>
-                      <NavItem href="#jumbotron" key={21}>Jumbotron</NavItem>
-                      <NavItem href="#page-header" key={22}>Page header</NavItem>
-                      <NavItem href="#responsive-embed" key={31}>Responsive embed</NavItem>
-                      <NavItem href="#wells" key={23}>Wells</NavItem>
-                      <NavItem href="#glyphicons" key={24}>Glyphicons</NavItem>
-                      <NavItem href="#tables" key={25}>Tables</NavItem>
-                      <NavItem href="#input" key={26}>Input</NavItem>
-                      <NavItem href="#utilities" key={28}>Utilities</NavItem>
-                      <SubNav href="#missing" key={32} text="Missing components">
-                        <NavItem href="#affix" key={33}>Affix</NavItem>
-                        <NavItem href="#scrollspy" key={34}>Scrollspy</NavItem>
+                      <NavItem href="#panels">Panels</NavItem>
+                      <NavItem href="#modals">Modals</NavItem>
+                      <NavItem href="#tooltips">Tooltips</NavItem>
+                      <NavItem href="#popovers">Popovers</NavItem>
+                      <NavItem href="#overlays">Overlays</NavItem>
+                      <NavItem href="#progress">Progress bars</NavItem>
+                      <NavItem href="#navs">Navs</NavItem>
+                      <NavItem href="#navbars">Navbars</NavItem>
+                      <NavItem href="#breadcrumbs">Breadcrumbs</NavItem>
+                      <NavItem href="#tabs">Tabs</NavItem>
+                      <NavItem href="#pager">Pager</NavItem>
+                      <NavItem href="#pagination">Pagination</NavItem>
+                      <NavItem href="#alerts">Alerts</NavItem>
+                      <NavItem href="#carousels">Carousels</NavItem>
+                      <NavItem href="#grids">Grids</NavItem>
+                      <NavItem href="#images">Images</NavItem>
+                      <NavItem href="#thumbnail">Thumbnail</NavItem>
+                      <NavItem href="#listgroup">List group</NavItem>
+                      <NavItem href="#labels">Labels</NavItem>
+                      <NavItem href="#badges">Badges</NavItem>
+                      <NavItem href="#jumbotron">Jumbotron</NavItem>
+                      <NavItem href="#page-header">Page header</NavItem>
+                      <NavItem href="#responsive-embed">Responsive embed</NavItem>
+                      <NavItem href="#wells">Wells</NavItem>
+                      <NavItem href="#glyphicons">Glyphicons</NavItem>
+                      <NavItem href="#tables">Tables</NavItem>
+                      <NavItem href="#input">Input</NavItem>
+                      <NavItem href="#utilities">Utilities</NavItem>
+                      <SubNav href="#missing" text="Missing components">
+                        <NavItem href="#affix">Affix</NavItem>
+                        <NavItem href="#scrollspy">Scrollspy</NavItem>
                       </SubNav>
                     </Nav>
                     <a className="back-to-top" href="#top">
