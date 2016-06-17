4902e2f40cda8da32a09c87ad14e2efd73c21dbb
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-06-17 10:21:10 -0700

    Add required id to tooltip examples

--- docs/examples/TooltipPositioned.js
+++ docs/examples/TooltipPositioned.js
@@ -1,6 +1,6 @@
 
 const tooltip = (
-  <Tooltip><strong>Holy guacamole!</strong> Check this info.</Tooltip>
+  <Tooltip id="tooltip"><strong>Holy guacamole!</strong> Check this info.</Tooltip>
 );
 
 const positionerInstance = (
