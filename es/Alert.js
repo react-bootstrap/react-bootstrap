e1e227ef23c4059120f0b6295d9d49bb71a1b86e
dimitrypatt <dimitry.patt@powtoon.com>
2016-11-15 13:30:23 +0200

    adding directories

---
+++ es/Alert.js
@@ -0,0 +1,96 @@
+import _Object$values from 'babel-runtime/core-js/object/values';
+import _extends from 'babel-runtime/helpers/extends';
+import _objectWithoutProperties from 'babel-runtime/helpers/objectWithoutProperties';
+import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
+import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructorReturn';
+import _inherits from 'babel-runtime/helpers/inherits';
+import classNames from 'classnames';
+import React from 'react';
+
+import { bsClass, bsStyles, getClassSet, prefix, splitBsProps } from './utils/bootstrapUtils';
+import { State } from './utils/StyleConfig';
+
+var propTypes = {
+  onDismiss: React.PropTypes.func,
+  closeLabel: React.PropTypes.string
+};
+
+var defaultProps = {
+  closeLabel: 'Close alert'
+};
+
+var Alert = function (_React$Component) {
+  _inherits(Alert, _React$Component);
+
+  function Alert() {
+    _classCallCheck(this, Alert);
+
+    return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
+  }
+
+  Alert.prototype.renderDismissButton = function renderDismissButton(onDismiss) {
+    return React.createElement(
+      'button',
+      {
+        type: 'button',
+        className: 'close',
+        onClick: onDismiss,
+        'aria-hidden': 'true',
+        tabIndex: '-1'
+      },
+      React.createElement(
+        'span',
+        null,
+        '\xD7'
+      )
+    );
+  };
+
+  Alert.prototype.renderSrOnlyDismissButton = function renderSrOnlyDismissButton(onDismiss, closeLabel) {
+    return React.createElement(
+      'button',
+      {
+        type: 'button',
+        className: 'close sr-only',
+        onClick: onDismiss
+      },
+      closeLabel
+    );
+  };
+
+  Alert.prototype.render = function render() {
+    var _extends2;
+
+    var _props = this.props,
+        onDismiss = _props.onDismiss,
+        closeLabel = _props.closeLabel,
+        className = _props.className,
+        children = _props.children,
+        props = _objectWithoutProperties(_props, ['onDismiss', 'closeLabel', 'className', 'children']);
+
+    var _splitBsProps = splitBsProps(props),
+        bsProps = _splitBsProps[0],
+        elementProps = _splitBsProps[1];
+
+    var dismissable = !!onDismiss;
+    var classes = _extends({}, getClassSet(bsProps), (_extends2 = {}, _extends2[prefix(bsProps, 'dismissable')] = dismissable, _extends2));
+
+    return React.createElement(
+      'div',
+      _extends({}, elementProps, {
+        role: 'alert',
+        className: classNames(className, classes)
+      }),
+      dismissable && this.renderDismissButton(onDismiss),
+      children,
+      dismissable && this.renderSrOnlyDismissButton(onDismiss, closeLabel)
+    );
+  };
+
+  return Alert;
+}(React.Component);
+
+Alert.propTypes = propTypes;
+Alert.defaultProps = defaultProps;
+
+export default bsStyles(_Object$values(State), State.INFO, bsClass('alert', Alert));
---
+++ es/MediaBody.js
@@ -0,0 +1,52 @@
+import _extends from 'babel-runtime/helpers/extends';
+import _objectWithoutProperties from 'babel-runtime/helpers/objectWithoutProperties';
+import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
+import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructorReturn';
+import _inherits from 'babel-runtime/helpers/inherits';
+import classNames from 'classnames';
+import React from 'react';
+import elementType from 'react-prop-types/lib/elementType';
+
+import { bsClass, getClassSet, splitBsProps } from './utils/bootstrapUtils';
+
+var propTypes = {
+  componentClass: elementType
+};
+
+var defaultProps = {
+  componentClass: 'div'
+};
+
+var MediaBody = function (_React$Component) {
+  _inherits(MediaBody, _React$Component);
+
+  function MediaBody() {
+    _classCallCheck(this, MediaBody);
+
+    return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
+  }
+
+  MediaBody.prototype.render = function render() {
+    var _props = this.props,
+        Component = _props.componentClass,
+        className = _props.className,
+        props = _objectWithoutProperties(_props, ['componentClass', 'className']);
+
+    var _splitBsProps = splitBsProps(props),
+        bsProps = _splitBsProps[0],
+        elementProps = _splitBsProps[1];
+
+    var classes = getClassSet(bsProps);
+
+    return React.createElement(Component, _extends({}, elementProps, {
+      className: classNames(className, classes)
+    }));
+  };
+
+  return MediaBody;
+}(React.Component);
+
+MediaBody.propTypes = propTypes;
+MediaBody.defaultProps = defaultProps;
+
+export default bsClass('media-body', MediaBody);
