a7fbb8ebc97bfb9bf2a4e2110c1e0062ecc7f66e
Jimmy Jia <tesrin@gmail.com>
2017-07-24 13:54:13 -0400

    Improve ButtonGroup docs

---
+++ docs/examples/ToggleButtonGroupControlled.js
@@ -0,0 +1,30 @@
+class ToggleButtonGroupControlled extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      value: [1, 3],
+    };
+  }
+
+  onChange = (value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    return (
+      <ToggleButtonGroup
+        type="checkbox"
+        value={this.state.value}
+        onChange={this.onChange}
+      >
+        <ToggleButton value={1}>Checkbox 1 (pre-checked)</ToggleButton>
+        <ToggleButton value={2}>Checkbox 2</ToggleButton>
+
+        <ToggleButton value={3}>Checkbox 3 (pre-checked)</ToggleButton>
+      </ToggleButtonGroup>
+    );
+  }
+}
+
+ReactDOM.render(<ToggleButtonGroupControlled />, mountNode);
