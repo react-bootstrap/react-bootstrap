47e21a3e0c0b6f58910aa6f5d5ec0b00b67c3e2e
Elad Levy <elad@bizzabo.com>
2017-10-20 16:02:53 +0300

    Add align prop to MediaBody (#2824) (#2830)

--- src/MediaBody.js
+++ src/MediaBody.js
@@ -1,10 +1,17 @@
 import classNames from 'classnames';
 import React from 'react';
+import PropTypes from 'prop-types';
 import elementType from 'prop-types-extra/lib/elementType';
 
-import { bsClass, getClassSet, splitBsProps } from './utils/bootstrapUtils';
+import Media from './Media';
+import { bsClass, getClassSet, prefix, splitBsProps } from './utils/bootstrapUtils';
 
 const propTypes = {
+  /**
+   * Align the media to the top, middle, or bottom of the media object.
+   */
+  align: PropTypes.oneOf(['top', 'middle', 'bottom']),
+
   componentClass: elementType,
 };
 
@@ -14,10 +21,15 @@ const defaultProps = {
 
 class MediaBody extends React.Component {
   render() {
-    const { componentClass: Component, className, ...props } = this.props;
+    const { componentClass: Component, align, className, ...props } = this.props;
     const [bsProps, elementProps] = splitBsProps(props);
 
     const classes = getClassSet(bsProps);
+
+    if (align) {
+      // The class is e.g. `media-top`, not `media-left-top`.
+      classes[prefix(Media.defaultProps, align)] = true;
+    }
 
     return (
       <Component
