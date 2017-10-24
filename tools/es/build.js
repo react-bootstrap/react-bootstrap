c83af5503138ab9a3340452a83de7114dda574b6
Damanpreet Singh <fumblehool@users.noreply.github.com>
2017-10-24 17:39:46 +0000

    Fixes #2831 added missing devDependencies (#2837)
    * copy the babelrc into build script
    
    * Update build.js
    
    * Update build.js
    
    * Update build.js

--- tools/es/build.js
+++ tools/es/build.js
@@ -12,15 +12,23 @@ export default function BuildES() {
     .then(() => buildBabel(srcRoot, esRoot, {
       babelrc: false,
       presets: [
-        ['es2015', { loose: true, modules: false }],
-        'stage-1',
+        ['env', {
+          loose: true,
+          modules: false,
+          targets: {
+            ie: 9,
+            uglify: true,
+          },
+        }],
         'react',
       ],
       plugins: [
+        'transform-class-properties',
+        'transform-object-rest-spread',
+        'transform-export-extensions',
         'dev-expression',
         'transform-runtime',
-        'transform-es3-member-expression-literals',
-        'transform-es3-property-literals',
+        'add-module-exports',
       ],
     }))
     .then(() => console.log('Built: '.cyan + 'es module'.green));
