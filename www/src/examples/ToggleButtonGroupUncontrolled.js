f37e032bff90c72367c364c2ff73b6217b811200
Tom Scott <tomscott85@gmail.com>
2018-01-23 22:03:34 +1100

    change onChange to handleChange and include as an instance property

--- www/src/examples/ToggleButtonGroupUncontrolled.js
+++ www/src/examples/ToggleButtonGroupUncontrolled.js
@@ -4,7 +4,6 @@ const toggleButtonGroups = (
       <ToggleButtonGroup type="checkbox" defaultValue={[1, 3]}>
         <ToggleButton value={1}>Checkbox 1 (pre-checked)</ToggleButton>
         <ToggleButton value={2}>Checkbox 2</ToggleButton>
-
         <ToggleButton value={3}>Checkbox 3 (pre-checked)</ToggleButton>
       </ToggleButtonGroup>
     </ButtonToolbar>
@@ -13,7 +12,6 @@ const toggleButtonGroups = (
       <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
         <ToggleButton value={1}>Radio 1 (pre-checked)</ToggleButton>
         <ToggleButton value={2}>Radio 2</ToggleButton>
-
         <ToggleButton value={3}>Radio 3</ToggleButton>
       </ToggleButtonGroup>
     </ButtonToolbar>
