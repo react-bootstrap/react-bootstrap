3668e72b05627412df1ebc9fcc6c6ddf06f70fc3
Oksana <unretrofied@yandex.ru>
2018-02-06 20:25:24 +0300

    Version usage clarification (#2981)

--- www/src/pages/getting-started/introduction.js
+++ www/src/pages/getting-started/introduction.js
@@ -47,6 +47,13 @@ $ bower install react-bootstrap
             and which bootstrap styles you include is up to you, but the
             simplest way is to include the latest styles from the CDN.
           </p>
+          <div className="bs-callout bs-callout-warning">
+            <p>
+              React-Bootstrap currently targets Bootstrap v3. To use
+              React-Bootstrap, include the CSS for Bootstrap v3 instead of
+              Bootstrap v4.
+            </p>
+          </div>
           <pre>
             <code>{`
 <!-- Latest compiled and minified CSS -->
@@ -73,9 +80,9 @@ $ bower install react-bootstrap
           </p>
           <div className="bs-callout bs-callout-warning">
             <p>
-              Because React-Bootstrap completely re-implements the JavaScript
-              bits of Bootstrap, it's not automatically compatible with themes
-              that extend the default JavaScript behaviors.
+              Because React-Bootstrap completely reimplements Bootstrap's
+              JavaScript, it's not automatically compatible with themes that
+              extend the default JavaScript behaviors.
             </p>
           </div>
           <h2 className="page-header">
