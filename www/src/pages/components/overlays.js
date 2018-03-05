4b550b28409ee51819e7e67418948787ba2eb5d0
Jason Quense <monastic.panic@gmail.com>
2018-03-05 12:30:17 -0500

    update to babel 7 (#3044)
    * update to babel 7
    
    * require

--- www/src/pages/components/overlays.js
+++ www/src/pages/components/overlays.js
@@ -34,8 +34,8 @@ export default function OverlaySection({ data }) {
         You don't need to use the provided <code>Tooltip</code> or{' '}
         <code>Popover</code> components. Creating custom overlays is as easy as
         wrapping some markup in an <code>Overlay</code> component. Make sure to
-        pass down the <code>className</code> and <code>style</code> props to
-        the wrapped element to make positioning and transitions work.
+        pass down the <code>className</code> and <code>style</code> props to the
+        wrapped element to make positioning and transitions work.
       </p>
       <ReactPlayground codeText={OverlayCustom} />
 
