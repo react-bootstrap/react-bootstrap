fb65d2c1d36fd95f2cc0ec2b3937881b75b47f8e
Jason Quense <monastic.panic@gmail.com>
2018-03-06 15:37:00 -0500

    fix docs so they build

--- www/src/examples/Fade.js
+++ www/src/examples/Fade.js
@@ -15,12 +15,9 @@ class Example extends React.Component {
         </Button>
         <Fade in={this.state.open}>
           <div>
-            <Well>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. Nihil anim keffiyeh
-              helvetica, craft beer labore wes anderson cred nesciunt sapiente
-              ea proident.
-            </Well>
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident.
           </div>
         </Fade>
       </div>
