75b7899bc3615d7bded3a726d3ff6c6d789129d0
grady-lad <ogradymartin7@gmail.com>
2018-03-12 09:09:05 +1100

    Removing `ProgressBar` prefix from progressBar examples

--- www/src/pages/components/progress.js
+++ www/src/pages/components/progress.js
@@ -5,13 +5,13 @@ import LinkToSource from '../../components/LinkToSource';
 import PropTable from '../../components/PropTable';
 import ReactPlayground from '../../components/ReactPlayground';
 
-import ProgressBarBasic from '../../examples/ProgressBar/ProgressBarBasic';
-import ProgressBarWithLabel from '../../examples/ProgressBar/ProgressBarWithLabel';
-import ProgressBarScreenreaderLabel from '../../examples/ProgressBar/ProgressBarScreenreaderLabel';
-import ProgressBarContextual from '../../examples/ProgressBar/ProgressBarContextual';
-import ProgressBarStriped from '../../examples/ProgressBar/ProgressBarStriped';
-import ProgressBarAnimated from '../../examples/ProgressBar/ProgressBarAnimated';
-import ProgressBarStacked from '../../examples/ProgressBar/ProgressBarStacked';
+import ProgressBarBasic from '../../examples/ProgressBar/Basic';
+import ProgressBarWithLabel from '../../examples/ProgressBar/WithLabel';
+import ProgressBarScreenreaderLabel from '../../examples/ProgressBar/ScreenreaderLabel';
+import ProgressBarContextual from '../../examples/ProgressBar/Contextual';
+import ProgressBarStriped from '../../examples/ProgressBar/Striped';
+import ProgressBarAnimated from '../../examples/ProgressBar/Animated';
+import ProgressBarStacked from '../../examples/ProgressBar/Stacked';
 
 export default function ProgressBarSection({ data }) {
   return (
