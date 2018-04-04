dafc814d04fa91667b3d8e5d332b976950987482
Jason Quense <monastic.panic@gmail.com>
2018-04-04 15:48:49 -0400

    Pr/3072 (#3107)
    * add alert heading
    
    * add bsStyle variants
    
    * Add props description
    
    * update

--- src/Alert.js
+++ src/Alert.js
@@ -1,55 +1,129 @@
 import classNames from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
+import elementType from 'prop-types-extra/lib/elementType';
+import uncontrollable from 'uncontrollable';
 
-import {
-  bsClass,
-  bsStyles,
-  getClassSet,
-  prefix,
-  splitBsProps
-} from './utils/bootstrapUtils';
-import { State } from './utils/StyleConfig';
+import createWithBsPrefix from './utils/createWithBsPrefix';
+import { createBootstrapComponent } from './ThemeProvider';
+import Fade from './Fade';
 import CloseButton from './CloseButton';
+import SafeAnchor from './SafeAnchor';
 
-const propTypes = {
-  onDismiss: PropTypes.func,
-  closeLabel: PropTypes.string
-};
-
-const defaultProps = {
-  closeLabel: 'Close alert'
-};
-
+/**
+ * @property {AlertHeading} Heading
+ * @property {AlertLink} Link
+ */
 class Alert extends React.Component {
+  static propTypes = {
+    /**
+     * @default 'alert'
+     */
+    bsPrefix: PropTypes.string,
+
+    /**
+     * The Alert visual variant
+     *
+     * @type('primary'|'secondary'|'success'|'danger'|'warning'|'info'|'dark'|'light')
+     */
+    variant: PropTypes.string,
+
+    /**
+     * Renders a properly aligned dismiss button, as well as
+     * adding extra horizontal padding to the Alert.
+     */
+    dismissible: PropTypes.bool,
+
+    /**
+     * Controls the visual state of the Alert.
+     *
+     * @controllable onClose
+     */
+    show: PropTypes.bool,
+
+    /**
+     * Callback fired when alert is closed.
+     *
+     * @controllable show
+     */
+    onClose: PropTypes.func,
+
+    /**
+     * Sets the text for alert close button.
+     */
+    closeLabel: PropTypes.string,
+
+    /** A `react-transition-group` Transition component used to animate the Alert on dismissal. */
+    transition: elementType
+  };
+
+  static defaultProps = {
+    show: true,
+    transition: Fade,
+    closeLabel: 'Close alert'
+  };
+
+  handleClose = e => {
+    this.props.onClose(false, e);
+  };
+
   render() {
-    const { onDismiss, closeLabel, className, children, ...props } = this.props;
-    const [bsProps, elementProps] = splitBsProps(props);
+    const {
+      bsPrefix,
+      show,
+      closeLabel,
+      className,
+      children,
+      variant,
+      dismissible,
+      transition: Transition,
+      onClose: _,
+      ...props
+    } = this.props;
 
-    const dismissable = !!onDismiss;
-    const classes = {
-      ...getClassSet(bsProps),
-      [prefix(bsProps, 'dismissable')]: dismissable
-    };
-
-    return (
+    const alert = (
       <div
-        {...elementProps}
         role="alert"
-        className={classNames(className, classes)}
+        {...(Transition ? props : undefined)}
+        className={classNames(
+          className,
+          bsPrefix,
+          variant && `${bsPrefix}-${variant}`,
+          dismissible && `${bsPrefix}-dismissible`
+        )}
       >
-        {dismissable && <CloseButton onClick={onDismiss} label={closeLabel} />}
+        {dismissible && (
+          <CloseButton onClick={this.handleClose} label={closeLabel} />
+        )}
         {children}
       </div>
+    );
+
+    if (!Transition) return show ? alert : null;
+
+    return (
+      <Transition unmountOnExit {...props} in={show}>
+        {alert}
+      </Transition>
     );
   }
 }
 
-Alert.propTypes = propTypes;
-Alert.defaultProps = defaultProps;
-
-export default bsStyles(
-  Object.values(State),
-  State.INFO,
-  bsClass('alert', Alert)
+const DecoratedAlert = uncontrollable(
+  createBootstrapComponent(Alert, 'alert'),
+  {
+    show: 'onClose'
+  }
 );
+
+const h4 = React.forwardRef((p, ref) => (
+  <div {...p} ref={ref} className={classNames(p.className, 'h4')} />
+));
+
+DecoratedAlert.Link = createWithBsPrefix('alert-link', {
+  Component: SafeAnchor
+});
+
+DecoratedAlert.Heading = createWithBsPrefix('alert-heading', { Component: h4 });
+
+export default DecoratedAlert;
--- www/src/pages/components/alerts.js
+++ www/src/pages/components/alerts.js
@@ -5,8 +5,10 @@ import LinkToSource from '../../components/LinkToSource';
 import PropTable from '../../components/PropTable';
 import ReactPlayground from '../../components/ReactPlayground';
 
-import AlertBasic from '../../examples/AlertBasic';
-import AlertDismissable from '../../examples/AlertDismissable';
+import Basic from '../../examples/Alert/Basic';
+import Link from '../../examples/Alert/Link';
+import Dismissible from '../../examples/Alert/Dismissible';
+import DismissibleControlled from '../../examples/Alert/DismissibleControlled';
 
 export default function AlertsSection({ data }) {
   return (
@@ -15,17 +17,34 @@ export default function AlertsSection({ data }) {
         <Anchor id="alerts">Alert messages</Anchor> <small>Alert</small>
       </h2>
 
-      <p>Basic alert styles.</p>
-      <ReactPlayground codeText={AlertBasic} />
-
+      <p>
+        Alerts are available for any length of text, as well as an optional
+        dismiss button. For proper styling, use one of the eight{' '}
+        <code>variant</code>s.
+      </p>
+      <ReactPlayground codeText={Basic} />
       <h3>
-        <Anchor id="alerts-closeable">Closeable alerts</Anchor>
+        <Anchor id="alerts-links">Links</Anchor>
       </h3>
       <p>
-        just pass in a <code>onDismiss</code> function.
+        For links, use the <code>Alert.Link</code> component to provide matching
+        colored links within any alert.
       </p>
-      <ReactPlayground codeText={AlertDismissable} />
+      <ReactPlayground codeText={Link} />
 
+      <h3>
+        <Anchor id="alerts-dismissible">Dismissible alerts</Anchor>
+      </h3>
+      <p>
+        Add the <code>dismissable</code> prop to add a functioning dismiss
+        button to the Alert.
+      </p>
+      <ReactPlayground codeText={Dismissible} />
+      <p>
+        You can also control the visual state directly which is great if you
+        wnat to build more complicated alerts.
+      </p>
+      <ReactPlayground codeText={DismissibleControlled} />
       <h3>
         <Anchor id="alert-props">Props</Anchor>
         <LinkToSource component={data.metadata.displayName} />
