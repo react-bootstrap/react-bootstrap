1f55d7e61f92f5cb226ddb03eb3e5aab54c0b7b7
Ken-Lauren Daganio <akosiken@gmail.com>
2018-04-19 14:54:56 +0800

    v4-listgroup: cleans up docs pages

--- www/src/pages/components/list-group.js
+++ www/src/pages/components/list-group.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import Anchor from '../../components/Anchor';
-import LinkToSource from '../../components/LinkToSource';
-import PropTable from '../../components/PropTable';
+import Heading from '../../components/Heading';
+import ComponentApi from '../../components/ComponentApi';
 import ReactPlayground from '../../components/ReactPlayground';
 
 import ListGroupDefault from '../../examples/ListGroupDefault';
@@ -15,61 +14,57 @@ import ListGroupCustom from '../../examples/ListGroupCustom';
 export default function ListGroupSection({ data }) {
   return (
     <div className="bs-docs-section">
-      <h2 className="page-header">
-        <Anchor id="listgroup">List group</Anchor>{' '}
-        <small>ListGroup, ListGroupItem</small>
-      </h2>
-
-      <p>
+      <Heading h="1" className="page-header" id="listgroup">
+        List group
+      </Heading>
+      <p className="lead">
         List groups are a flexible and powerful component for displaying not
         only simple lists of elements, but complex ones with custom content.
       </p>
 
-      <h3>
-        <Anchor id="listgroup-default">Centers by default</Anchor>
-      </h3>
+      <Heading h="2" id="listgroup-example-default">
+        Basic Example
+      </Heading>
       <ReactPlayground codeText={ListGroupDefault} />
 
-      <h3>
-        <Anchor id="listgroup-linked">Linked</Anchor>
-      </h3>
+      <Heading h="3" id="listgroup-example-linked">
+        Linked
+      </Heading>
       <p>
         Set the <code>href</code> or <code>onClick</code> prop on{' '}
         <code>ListGroupItem</code>, to create a linked or clickable element.
       </p>
       <ReactPlayground codeText={ListGroupLinked} />
 
-      <h3>
-        <Anchor id="listgroup-styling-state">Styling by state</Anchor>
-      </h3>
+      <Heading h="3" id="listgroup-styling-state">
+        Styling by state
+      </Heading>
       <p>
         Set the <code>active</code> or <code>disabled</code> prop to{' '}
         <code>true</code> to mark or disable the item.
       </p>
       <ReactPlayground codeText={ListGroupActive} />
 
-      <h3>
-        <Anchor id="listgroup-styling-color">Styling by color</Anchor>
-      </h3>
+      <Heading h="3" id="listgroup-styling-color">
+        Styling by color
+      </Heading>
       <p>
         Set the <code>bsStyle</code> prop to style the item
       </p>
       <ReactPlayground codeText={ListGroupStyle} />
 
-      <h3>
-        <Anchor id="listgroup-with-header">With header</Anchor>
-      </h3>
+      <Heading h="3" id="listgroup-with-header">
+        With header
+      </Heading>
       <p>
         Set the <code>header</code> prop to create a structured item, with a
         heading and a body area.
       </p>
       <ReactPlayground codeText={ListGroupHeader} />
 
-      <h3>
-        <Anchor id="listgroup-with-custom-children">
-          With custom component children
-        </Anchor>
-      </h3>
+      <Heading h="3" id="listgroup-with-custom-children">
+        With custom component children
+      </Heading>
       <p>
         When using ListGroupItems directly, ListGroup looks at whether the items
         have href or onClick props to determine which DOM elements to emit.
@@ -80,19 +75,12 @@ export default function ListGroupSection({ data }) {
       </p>
       <ReactPlayground codeText={ListGroupCustom} />
 
-      <h3>
-        <Anchor id="listgroup-props">Props</Anchor>
-      </h3>
+      <Heading h="2" id="card-props">
+        Props
+      </Heading>
 
-      <h4>
-        <Anchor id="listgroup-props-group">ListGroup</Anchor>
-      </h4>
-      <PropTable metadata={data.ListGroup} />
-
-      <h4>
-        <Anchor id="listgroup-props-item">ListGroupItem</Anchor>
-        <LinkToSource component={data.ListGroupItem.displayName} />
-      </h4>
+      <ComponentApi metadata={data.ListGroup} />
+      <ComponentApi metadata={data.ListGroupItem} />
     </div>
   );
 }
