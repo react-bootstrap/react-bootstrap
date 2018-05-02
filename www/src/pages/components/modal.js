ea5734fd7c4c01ab4f2050f37d9d6f359342451f
Timothy Hwang <Tim.Hwang@sizmek.com>
2018-05-02 15:54:32 -0400

    Add warning about multiple modals not supported out of box
    Addresses #3130.

--- www/src/pages/components/modal.js
+++ www/src/pages/components/modal.js
@@ -90,6 +90,24 @@ export default function ModalSection({ data }) {
       <ReactPlayground codeText={ModalCustomSizing} />
 
       <h3>
+        <Anchor id="modals-multiple">Multiple Modals</Anchor>
+      </h3>
+      <div className="bs-callout bs-callout-warning">
+        <h4>
+          Not supported
+        </h4>
+        <p>
+          React-Bootstrap modals are not designed to support rendering multiple
+          modals simultaneously. You will have to add <code>react-overlays</code> as
+          a dependency and build your own modal component using its{' '}
+          <a href="https://github.com/react-bootstrap/react-overlays/blob/master/src/ModalManager.js">
+            <code>{'<ModalManager/>'}</code>
+          </a>{' '}
+          component, which supports multiple modals.
+        </p>
+      </div>
+
+      <h3>
         <Anchor id="modals-props">Props</Anchor>
       </h3>
 
