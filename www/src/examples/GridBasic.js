3972248b2bb9b0e7339b0b489f005f81262c4dd2
BANO.notIT <BANOnotIT@users.noreply.github.com>
2018-05-19 00:57:57 +0300

    Show < and > instead of &lt; and &gt;

--- www/src/examples/GridBasic.js
+++ www/src/examples/GridBasic.js
@@ -1,37 +1,37 @@
 <Grid>
   <Row className="show-grid">
     <Col xs={12} md={8}>
-      <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
+      <code>{'<Col xs={12} md={8} />'};</code>
     </Col>
     <Col xs={6} md={4}>
-      <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+      <code>{'<Col xs={6} md={4} />'}</code>
     </Col>
   </Row>
 
   <Row className="show-grid">
     <Col xs={6} md={4}>
-      <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+      <code>{'<Col xs={6} md={4} />'}</code>
     </Col>
     <Col xs={6} md={4}>
-      <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+      <code>{'<Col xs={6} md={4} />'}</code>
     </Col>
     <Col xsHidden md={4}>
-      <code>&lt;{'Col xsHidden md={4}'} /&gt;</code>
+      <code>{'<Col xsHidden md={4} />'}</code>
     </Col>
   </Row>
 
   <Row className="show-grid">
     <Col xs={6} xsOffset={6}>
-      <code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code>
+      <code>{'<Col xs={6} xsOffset={6} />'}</code>
     </Col>
   </Row>
 
   <Row className="show-grid">
     <Col md={6} mdPush={6}>
-      <code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code>
+      <code>{'<Col md={6} mdPush={6} />'}</code>
     </Col>
     <Col md={6} mdPull={6}>
-      <code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code>
+      <code>{'<Col md={6} mdPull={6} />'}</code>
     </Col>
   </Row>
 </Grid>;
