68c164efc2ece706b144c760c9c4bc87e1764b26
Jason Quense <monastic.panic@gmail.com>
2018-08-21 14:39:17 -0400

    GO!

--- www/src/layouts/index.js
+++ www/src/layouts/index.js
@@ -1,8 +1,17 @@
+import { MDXProvider } from '@mdx-js/tag';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import NavMain from '../components/NavMain';
+import Anchor from '../components/Anchor';
 
+const components = {
+  h1: props => <Anchor as="h1" {...props} />,
+  h2: props => <Anchor as="h2" {...props} />,
+  h3: props => <Anchor as="h3" {...props} />,
+  h4: props => <Anchor as="h4" {...props} />,
+  h5: props => <Anchor as="h5" {...props} />,
+};
 const propTypes = {
   location: PropTypes.object.isRequired,
 };
@@ -11,8 +20,7 @@ function DefaultLayout({ children, location }) {
   return (
     <div>
       <NavMain activePage={location.pathname} />
-
-      {children}
+      <MDXProvider components={components}>{children}</MDXProvider>
 
       {/* <PageFooter /> */}
     </div>
