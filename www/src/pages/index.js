78f05b7a90ad82abfb4ddda41181d8b7e6cb5d36
Joni Sar <yoni@cocycles.com>
2018-10-23 15:56:01 +0300

    docs: update index.js / Fix grammer (stratch -> scratch) (#3345)
    Fixed to "built from scratch" instead of "stratch", which is a grammar mistake in this context.
    
    Cheers :)

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -137,7 +137,7 @@ export default withLayout(
                 <h2>Rebuilt with React</h2>
                 <p>
                   React bootstrap replaces the Bootstrap javascript. Each
-                  component has been built from stratch as true React
+                  component has been built from scratch as true React
                   components, without uneeded dependencies like jQuery.
                 </p>
                 <p>
