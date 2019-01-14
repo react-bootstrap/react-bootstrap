e0763dbaeef56d8c989b0dc8fcd3f059d73f70da
Glen Mailer <glen@stainlessed.co.uk>
2019-01-14 17:35:04 +0000

    Fix weird CI error

--- types/simple.test.tsx
+++ types/simple.test.tsx
@@ -171,7 +171,7 @@ import {
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlTextarea1">
     <Form.Label>Example textarea</Form.Label>
-    <Form.Control as="textarea" rows="3" />
+    <Form.Control as="textarea" rows={3} />
   </Form.Group>
 </Form>;
 
