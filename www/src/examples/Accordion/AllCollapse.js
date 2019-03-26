2c83de832ef6ec3b0b1cace70b180b8e036e5f07
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-03-25 22:11:01 -0700

    update Accordion doc to have an example of fully-collapsed state

---
+++ www/src/examples/Accordion/AllCollapse.js
@@ -0,0 +1,32 @@
+<Accordion>
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle eventKey="0">
+        <button
+          type="button"
+          onClick={key => console.log(`I've been clicked by : ${key}`)}
+        >
+          Click me!
+        </button>
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="0">
+      <Card.Body>Hello! I'm the body</Card.Body>
+    </Accordion.Collapse>
+  </Card>
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle eventKey="1">
+        <button
+          type="button"
+          onClick={key => console.log(`I've been clicked by : ${key}`)}
+        >
+          Click me!
+        </button>
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="1">
+      <Card.Body>Hello! I'm another body</Card.Body>
+    </Accordion.Collapse>
+  </Card>
+</Accordion>;
