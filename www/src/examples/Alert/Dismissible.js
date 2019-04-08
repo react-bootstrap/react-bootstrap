be177819f8983a824edc812d94512b369188f302
Max Schmitt <max@schmitt.mx>
2019-04-08 21:33:48 +0200

    feat: enhanced Alert dismissible example (#3599) (#3602)

--- www/src/examples/Alert/Dismissible.js
+++ www/src/examples/Alert/Dismissible.js
@@ -1,8 +1,28 @@
-<Alert dismissible variant="danger">
-  <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-  <p>
-    Change this and that and try again. Duis mollis, est non commodo luctus,
-    nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis
-    consectetur purus sit amet fermentum.
-  </p>
-</Alert>;
+class AlertDismissibleExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+    };
+  }
+
+  render() {
+    const handleDismiss = () => this.setState({ show: false });
+    const handleShow = () => this.setState({ show: true });
+    if (this.state.show) {
+      return (
+        <Alert variant="danger" onClose={handleDismiss} dismissible>
+          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+          <p>
+            Change this and that and try again. Duis mollis, est non commodo
+            luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+            Cras mattis consectetur purus sit amet fermentum.
+          </p>
+        </Alert>
+      );
+    }
+    return <Button onClick={handleShow}>Show Alert</Button>;
+  }
+}
+
+render(<AlertDismissibleExample />);
