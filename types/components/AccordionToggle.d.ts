c2cd5a20315630d098411d129c73ffb88cb42202
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-04-10 13:44:45 -0700

    fix type for AccordionToggle

--- types/components/AccordionToggle.d.ts
+++ types/components/AccordionToggle.d.ts
@@ -1,14 +1,14 @@
 import * as React from 'react';
-
+import Button from './Button';
 import { BsPrefixComponent } from './helpers';
 
 export interface AccordionToggleProps {
   eventKey: string;
-  onClick: (arg0?: React.SyntheticEvent) => any;
+  onClick?: (arg0?: React.SyntheticEvent) => any;
 }
 
 declare class AccordionToggle<
-  As extends React.ReactType = 'button'
-> extends BsPrefixComponent<As, AccordionToggleProps> {}
+  As extends React.ReactType = typeof Button
+  > extends BsPrefixComponent<As, AccordionToggleProps> { }
 
 export default AccordionToggle;
