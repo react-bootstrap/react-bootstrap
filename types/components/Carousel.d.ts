b74a9c5b3c1616d6701c1c37f0fb4a2c38343e79
sedrubal <sedrubal@users.noreply.github.com>
2019-04-15 16:03:32 +0200

    Allow null for Carousel interval in Typescript, fix typo in docs (#3652)
    The docs in https://github.com/react-bootstrap/react-bootstrap/blob/85550d39e07071322a43de79d28547611c036dae/src/Carousel.js#L43-L47 describe the behavior of setting `interval` to `null`, but `null` is missing in the typescript type definition.

--- types/components/Carousel.d.ts
+++ types/components/Carousel.d.ts
@@ -11,7 +11,7 @@ export interface CarouselProps {
   fade?: boolean;
   wrap?: boolean;
   indicators?: boolean;
-  interval?: number;
+  interval?: number | null;
   controls?: boolean;
   pauseOnHover?: boolean;
   keyboard?: boolean;
