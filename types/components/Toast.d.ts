248db87504ae353b84ff6dbccc861afd49c03f65
Max Schmitt <max@schmitt.mx>
2019-04-30 14:23:54 +0200

    fix: types

--- types/components/Toast.d.ts
+++ types/components/Toast.d.ts
@@ -2,17 +2,14 @@ import * as React from 'react';
 
 import ToastBody from './ToastBody';
 import ToastHeader from './ToastHeader';
-import ToastDialog from './ToastDialog';
 
 import {
   BsPrefixComponent,
-  SelectCallback,
   TransitionCallbacks,
 } from './helpers';
 
 export interface ToastProps extends TransitionCallbacks {
   animation?: boolean;
-  dialogAs?: React.ReactType;
   autohide?: boolean;
   delay?: number;
   onClose?: () => void;
@@ -20,13 +17,9 @@ export interface ToastProps extends TransitionCallbacks {
   transition: boolean | React.ElementType,
 }
 
-declare class Toast<
-  As extends React.ReactType = 'div'
-  > extends BsPrefixComponent<As, ToastProps> {
+declare class Toast extends BsPrefixComponent<'div', ToastProps> {
   static Body: typeof ToastBody;
   static Header: typeof ToastHeader;
-
-  static Dialog: typeof ToastDialog;
 }
 
 export default Toast;
