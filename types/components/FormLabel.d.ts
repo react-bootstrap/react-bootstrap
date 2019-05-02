153ff6a8f39f1cfc416dc6d210e654822f1cb22e
Dale Fenton <fentondale@gmail.com>
2019-05-02 08:42:31 -0400

    type FormLabelProps with type union

--- types/components/FormLabel.d.ts
+++ types/components/FormLabel.d.ts
@@ -3,12 +3,21 @@ import * as React from 'react';
 import { BsPrefixComponent } from './helpers';
 import { ColProps } from './Col';
 
-export interface FormLabelProps extends ColProps {
+interface FormLabelBaseProps {
   htmlFor?: string;
-  column?: boolean;
   innerRef?: React.LegacyRef<this>;
   srOnly?: boolean;
 }
+
+export interface FormLabelOwnProps extends FormLabelBaseProps {
+  column?: false;
+}
+
+export interface FormLabelWithColProps extends FormLabelBaseProps, ColProps {
+  column: true;
+}
+
+export type FormLabelProps = FormLabelWithColProps|FormLabelOwnProps
 
 declare class FormLabel extends BsPrefixComponent<'label', FormLabelProps> {}
 
