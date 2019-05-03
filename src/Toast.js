ebc53eb2c29c232dd9880bff6b255f3e97afa701
Max Schmitt <max@schmitt.mx>
2019-05-03 23:06:45 +0200

    fix: adjusted coding related to the review

--- src/Toast.js
+++ src/Toast.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -56,78 +56,54 @@ const defaultProps = {
   transition: Fade,
 };
 
-class Toast extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toastContext = {
-      onClose: () => this.props.onClose(),
-    };
-    const { autohide } = props;
-    if (autohide) {
-      this.scheduleAutohideTimeout(props.delay);
+const Toast = ({
+  bsPrefix,
+  className,
+  children,
+  transition: Transition,
+  show,
+  animation,
+  delay,
+  autohide,
+  onClose,
+  innerRef,
+  ...props
+}) => {
+  useEffect(() => {
+    if (autohide && show) {
+      setTimeout(() => {
+        onClose();
+      }, delay);
     }
-  }
+  });
+  const useAnimation = Transition && animation;
+  const toast = (
+    <div
+      {...props}
+      ref={innerRef}
+      className={classNames(
+        bsPrefix,
+        className,
+        !useAnimation && show && 'show',
+      )}
+      role="alert"
+      aria-live="assertive"
+      aria-atomic="true"
+    >
+      {children}
+    </div>
+  );
 
-  componentDidUpdate(prevProps) {
-    const { autohide, delay, show } = this.props;
-    if (!this.autohideInterval && autohide && show !== prevProps.show) {
-      this.scheduleAutohideTimeout(delay);
-    }
-  }
-
-  scheduleAutohideTimeout = delay => {
-    this.autohideInterval = setTimeout(() => {
-      this.props.onClose();
-      this.autohideInterval = null;
-    }, delay);
+  const toastContext = {
+    onClose: () => onClose(),
   };
 
-  render() {
-    const {
-      bsPrefix,
-      className,
-      children,
-      transition: Transition,
-      show,
-      animation,
-      delay: _delay,
-      autohide: _autohide,
-      innerRef,
-      ...props
-    } = this.props;
-
-    const useAnimation = Transition && animation;
-    const toast = (
-      <div
-        {...props}
-        ref={innerRef}
-        className={classNames(
-          bsPrefix,
-          className,
-          !useAnimation && show && 'show',
-        )}
-        role="alert"
-        aria-live="assertive"
-        aria-atomic="true"
-      >
-        {children}
-      </div>
-    );
-
-    if (useAnimation) {
-      return (
-        <ToastContext.Provider value={this.toastContext}>
-          <Transition in={show}>{toast}</Transition>
-        </ToastContext.Provider>
-      );
-    }
-    return (
-      <ToastContext.Provider value={this.toastContext}>
-        {toast}
-      </ToastContext.Provider>
-    );
-  }
-}
+  return (
+    <ToastContext.Provider value={toastContext}>
+      {useAnimation ? <Transition in={show}>{toast}</Transition> : toast}
+    </ToastContext.Provider>
+  );
+};
 
 Toast.propTypes = propTypes;
 Toast.defaultProps = defaultProps;
