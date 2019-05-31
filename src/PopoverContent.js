4f267205d58f9690324f1fbf4e1c33dc9f7a42d4
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-31 01:29:13 -0700

    Add 'as' prop to PopoverContent and PopoverTitle
    this will allow users to define a custom element type for their
    Popover components (such as setting PopoverTitle to use an 'h3'
    element)

--- src/PopoverContent.js
+++ src/PopoverContent.js
@@ -4,18 +4,35 @@ import PropTypes from 'prop-types';
 import { useBootstrapPrefix } from './ThemeProvider';
 
 const propTypes = {
+  /** Set a custom element for this component */
+  as: PropTypes.elementType,
+
   /** @default 'popover-body' */
   bsPrefix: PropTypes.string,
 };
 
 const PopoverContent = React.forwardRef(
-  ({ bsPrefix, className, children, ...props }, ref) => {
+  (
+    {
+      // Need to define the default "as" during prop destructuring to be compatible with styled-components github.com/react-bootstrap/react-bootstrap/issues/3595
+      as: Component = 'div',
+      bsPrefix,
+      className,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
     bsPrefix = useBootstrapPrefix(bsPrefix, 'popover-body');
 
     return (
-      <div ref={ref} {...props} className={classNames(className, bsPrefix)}>
+      <Component
+        ref={ref}
+        {...props}
+        className={classNames(className, bsPrefix)}
+      >
         {children}
-      </div>
+      </Component>
     );
   },
 );
