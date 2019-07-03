f53405fac0415ac636c5a539bb7f9c48f049b78a
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-07-02 19:46:24 -0700

    fix: separate state variables for Controlled example of Carousel

--- www/src/examples/Carousel/Controlled.js
+++ www/src/examples/Carousel/Controlled.js
@@ -1,18 +1,14 @@
 function ControlledCarousel() {
-  const [state, setState] = useState({ index: 0, direction: null });
+  const [index, setIndex] = useState(0);
+  const [direction, setDirection] = useState(null);
 
-  const handleSelect = (selectedIndex, e) =>
-    setState({
-      index: selectedIndex,
-      direction: e.direction,
-    });
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+    setDirection(e.direction);
+  };
 
   return (
-    <Carousel
-      activeIndex={state.index}
-      direction={state.direction}
-      onSelect={handleSelect}
-    >
+    <Carousel activeIndex={index} direction={direction} onSelect={handleSelect}>
       <Carousel.Item>
         <img
           className="d-block w-100"
