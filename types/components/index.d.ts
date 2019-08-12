b77d686d6aafa24ecfbef6e441e5018280240326
morpha <ngx.morpha@gmail.com>
2019-08-13 01:46:40 +1000

    Update index.d.ts (#4230)
    * Update index.d.ts
    
    Added missing references to Toast Components Type definitions.
    
    * Update index.d.ts

--- types/components/index.d.ts
+++ types/components/index.d.ts
@@ -109,6 +109,9 @@ export { default as Table, TableProps } from './Table';
 export { default as TabPane, TabPaneProps } from './TabPane';
 export { default as Tabs, TabsProps } from './Tabs';
 export { default as ThemeProvider, ThemeProviderProps } from './ThemeProvider';
+export { default as Toast, ToastProps } from './Toast';
+export { default as ToastBody } from './ToastBody';
+export { default as ToastHeader, ToastHeaderProps } from './ToastHeader';
 export { default as ToggleButton, ToggleButtonProps } from './ToggleButton';
 export {
   default as ToggleButtonGroup,
