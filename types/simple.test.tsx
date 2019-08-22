46fcf7ebb461fd54fa86a163bbc11eb1fa8ba5be
Max Schmitt <max@schmitt.mx>
2019-08-22 08:32:13 +0200

    fix: prettier

--- types/simple.test.tsx
+++ types/simple.test.tsx
@@ -56,7 +56,9 @@ import {
 
 <Badge pill={false}>42</Badge>;
 
-<Badge as="a" href="#" variant="primary" pill>42</Badge>;
+<Badge as="a" href="#" variant="primary" pill>
+  42
+</Badge>;
 
 <Breadcrumb listProps={{ type: 'I' }}>
   <Breadcrumb.Item />
