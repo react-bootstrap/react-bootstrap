50139794c99f17f50e0d9268dfa206147a9c7aa0
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-12-09 23:02:31 +0800

    fix failing buid

--- src/Tabs.js
+++ src/Tabs.js
@@ -11,7 +11,7 @@ import TabContainer from './TabContainer';
 import TabContent from './TabContent';
 import TabPane from './TabPane';
 
-import { forEach, map } from './ElementChildren';
+import { map } from './ElementChildren';
 
 const propTypes = {
   /**
@@ -83,8 +83,8 @@ const defaultProps = {
 };
 
 function getDefaultActiveKey(children) {
-  const child = children.find(child => child.props.eventKey);
-  return child ? child.props.eventKey : null;
+  const first = children.find(child => child.props.eventKey);
+  return first ? first.props.eventKey : null;
 }
 
 function renderTab(child) {
