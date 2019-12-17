92e7ef5fc16cca1c46f37581d010a39e2ceb3dec
Tor Raswill <tor.raswill@dqc.se>
2019-12-17 13:23:03 +0100

    Update Container.js
    Add breakpoint prop, set accepted values for prop, add use of prop as suffix if fluid is false

--- src/Container.js
+++ src/Container.js
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 
 import { useBootstrapPrefix } from './ThemeProvider';
 
+const brkPoints = PropTypes.oneOfType([
+  PropTypes.oneOf(['sm', 'md', 'lg', 'xl']),
+]);
+
 const propTypes = {
   /**
    * @default 'container'
@@ -18,6 +22,10 @@ const propTypes = {
    * You can use a custom element for this component
    */
   as: PropTypes.elementType,
+  /**
+   * You can set responsive container width.
+   */
+  breakpoint: brkPoints,
 };
 
 const defaultProps = {
@@ -26,13 +34,20 @@ const defaultProps = {
 
 const Container = React.forwardRef(
   // Need to define the default "as" during prop destructuring to be compatible with styled-components github.com/react-bootstrap/react-bootstrap/issues/3595
-  ({ bsPrefix, fluid, as: Component = 'div', className, ...props }, ref) => {
+  (
+    { bsPrefix, fluid, breakpoint, as: Component = 'div', className, ...props },
+    ref,
+  ) => {
     const prefix = useBootstrapPrefix(bsPrefix, 'container');
+    let suffix = breakpoint != null ? `-${breakpoint}` : '';
     return (
       <Component
         ref={ref}
         {...props}
-        className={classNames(className, fluid ? `${prefix}-fluid` : prefix)}
+        className={classNames(
+          className,
+          fluid ? `${prefix}-fluid` : `${prefix}${suffix}`,
+        )}
       />
     );
   },
