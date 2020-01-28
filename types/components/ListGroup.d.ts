d0ea63837422c931e9112fcef234d6959a5c463c
mustangore <mustangore@gmail.com>
2020-01-28 16:52:40 +0100

    Add missing horizontal in ListGroupProps (#4864)

--- types/components/ListGroup.d.ts
+++ types/components/ListGroup.d.ts
@@ -6,6 +6,7 @@ import { BsPrefixComponent, SelectCallback } from './helpers';
 
 export interface ListGroupProps {
   variant?: 'flush';
+  horizontal?: boolean;
   activeKey?: unknown;
   defaultActiveKey?: unknown;
   onSelect?: SelectCallback;
