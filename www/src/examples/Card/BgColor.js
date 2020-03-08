e190af5a6bee93105235f567a45eeb9cf9a1ea27
Mohamed Nainar <mdnainar9615@gmail.com>
2020-03-08 08:54:03 +0530

    Made it *Prettier*

--- www/src/examples/Card/BgColor.js
+++ www/src/examples/Card/BgColor.js
@@ -1,22 +1,34 @@
 [
-  'primary',
-  'secondary',
-  'success',
-  'danger',
-  'warning',
-  'info',
-  'light',
-  'dark'
+  "primary",
+  "secondary",
+  "success",
+  "danger",
+  "warning",
+  "info",
+  "light",
+  "dark"
 ].map((variant, idx) => (
-  <><Card bg={variant} key={idx} text={variant==='light' ? 'dark' : 'white'}  style={{ width: '18rem' }}>
-    <Card.Header>Header</Card.Header>
-    <Card.Body>
-      <Card.Title>{variant} Card Title </Card.Title>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <br /></>
-  ))
+  <>
+    <Card
+      bg={variant}
+      key={idx}
+      text={variant === "light" ? "dark" : "white"}
+      style={{ width: "18rem" }}
+    >
+      <Card.Header>Header</Card.Header>
+      <Card.Body>
+        <Card.Title>{variant} Card Title </Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <br />
+  </>
+));
+
+
+
+
+
