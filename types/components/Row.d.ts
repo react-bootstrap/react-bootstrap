202d80bf3b2eccea56207d21f51f84ec99b20670
Tor Raswill <tor.raswill@dqc.se>
2020-03-09 14:20:14 +0100

    Add types to Row.d.ts
    Add types for rowColumns

--- types/components/Row.d.ts
+++ types/components/Row.d.ts
@@ -2,8 +2,29 @@ import * as React from 'react';
 
 import { BsPrefixComponent } from './helpers';
 
+type NumberAttr =
+  | number
+  | '1'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | '10'
+  | '11'
+  | '12';
+type RowColSpec = NumberAttr | { cols?: NumberAttr };
+
 export interface RowProps {
   noGutters?: boolean;
+  xs?: RowColSpec;
+  sm?: RowColSpec;
+  md?: RowColSpec;
+  lg?: RowColSpec;
+  xl?: RowColSpec;
 }
 
 declare class Row<
