d227deaa15ea8150e5629214b4bb0261cc66c98f
Tor Raswill <tor.raswill@dqc.se>
2020-03-30 10:32:56 +0200

    Update MasterDocsAlert.js
    just a minor test update since the alert was replaced with a container on netlify. Testing updated markup.

--- www/src/components/MasterDocsAlert.js
+++ www/src/components/MasterDocsAlert.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 function MasterDocsAlert() {
   return (
     typeof window !== 'undefined' &&
     window.location.host.substr(window.location.host.indexOf('.') + 1) ===
       'netlify.com' && (
-      <Alert variant="warning">
-        You are currently viewing the auto-generated docs from the
-        master-branch. The docs for the current release are available at{' '}
-        <Alert.Link href="https://react-bootstrap.github.io/">
-          https://react-bootstrap.github.io/
-        </Alert.Link>
-      </Alert>
+      <Container fluid>
+        <Row>
+          <Alert variant="warning" className="w-100">
+            You are currently viewing the auto-generated docs from the
+            master-branch. The docs for the current release are available at{' '}
+            <Alert.Link href="https://react-bootstrap.github.io/">
+              https://react-bootstrap.github.io/
+            </Alert.Link>
+          </Alert>
+        </Row>
+      </Container>
     )
   );
 }
