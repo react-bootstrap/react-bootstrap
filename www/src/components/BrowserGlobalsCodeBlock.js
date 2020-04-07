951ebbce6d4ed4f11bb7ccd8210cac72d8b8738f
armonkahil <armonkahil@gmail.com>
2020-04-07 00:52:42 -0400

    :wrench: changed self-closing script tags to closing script tags

--- www/src/components/BrowserGlobalsCodeBlock.js
+++ www/src/components/BrowserGlobalsCodeBlock.js
@@ -8,17 +8,15 @@ function BrowserGlobalsCodeBlock() {
     <CodeBlock
       mode="html"
       codeText={`
-<script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin />
+<script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
 
 <script
   src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-  crossorigin
-/>
+  crossorigin></script>
 
 <script
   src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-  crossorigin
-/>
+  crossorigin></script>
 
 <script>var Alert = ReactBootstrap.Alert;</script>
 
