b218c631062c71f1f0ef44881d190ddd12d1cbe5
ThunderMiracle <cissoliu@gmail.com>
2020-05-19 23:37:53 +0900

    [docs] Fix FontAwesome icons' flash when press F5 (#5160)

--- www/gatsby-browser.js
+++ www/gatsby-browser.js
@@ -1,3 +1,10 @@
 require('./src/css/global.scss');
 
+// prevent FontAwesome icons' flash from a very large one down to a properly sized one
+require('@fortawesome/fontawesome-svg-core/styles.css');
+// Prevent fontawesome from adding its CSS since we did it manually above
+const { config } = require('@fortawesome/fontawesome-svg-core');
+
+config.autoAddCss = false;
+
 exports.wrapPageElement = require(`./src/wrap-page`);
