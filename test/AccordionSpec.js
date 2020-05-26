3b520a6ca3924014944ae22781056e10c2be021d
kyletsang <6854874+kyletsang@users.noreply.github.com>
2020-05-26 12:56:30 -0700

    fix(Accordion): Use empty SelectableContext to prevent dropdown from closing accordion

--- test/AccordionSpec.js
+++ test/AccordionSpec.js
@@ -3,6 +3,9 @@ import { mount } from 'enzyme';
 
 import Accordion from '../src/Accordion';
 import Card from '../src/Card';
+import Dropdown from '../src/Dropdown';
+import ListGroup from '../src/ListGroup';
+import Nav from '../src/Nav';
 
 describe('<Accordion>', () => {
   it('should output a div', () => {
@@ -72,5 +75,94 @@ describe('<Accordion>', () => {
     // (which is possible in `@testing-library` via `waitForElement`).
     // https://testing-library.com/docs/dom-testing-library/api-async#waitforelement
     collapses.at(1).getDOMNode().className.should.include('collapsing');
+  });
+
+  // https://github.com/react-bootstrap/react-bootstrap/issues/4176
+  it('Should not close accordion when child dropdown clicked', () => {
+    const wrapper = mount(
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle eventKey="0" />
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <Dropdown show>
+                <Dropdown.Toggle id="dropdown-test">
+                  Dropdown Button
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#">Action</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>,
+    );
+
+    wrapper.find('DropdownItem').simulate('click');
+
+    wrapper
+      .find('AccordionCollapse')
+      .at(0)
+      .getDOMNode()
+      .className.should.include('show');
+  });
+
+  it('Should not close accordion when child ListGroup clicked', () => {
+    const wrapper = mount(
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle eventKey="0" />
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <div>
+              <ListGroup defaultActiveKey="#link1">
+                <ListGroup.Item action href="#link1">
+                  Link 1
+                </ListGroup.Item>
+              </ListGroup>
+            </div>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>,
+    );
+
+    wrapper.find('ListGroupItem').simulate('click');
+
+    wrapper
+      .find('AccordionCollapse')
+      .at(0)
+      .getDOMNode()
+      .className.should.include('show');
+  });
+
+  it('Should not close accordion when child Nav clicked', () => {
+    const wrapper = mount(
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle eventKey="0" />
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Nav activeKey="/home">
+              <Nav.Item>
+                <Nav.Link href="#">Active</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>,
+    );
+
+    wrapper.find('NavLink').simulate('click');
+
+    wrapper
+      .find('AccordionCollapse')
+      .at(0)
+      .getDOMNode()
+      .className.should.include('show');
   });
 });
