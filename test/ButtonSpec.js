260b73437f18d425f1b7c5eda7ea795989a33cb2
kyletsang <6854874+kyletsang@users.noreply.github.com>
2020-06-01 10:04:34 -0700

    Use type prop if explicitly passed in

--- test/ButtonSpec.js
+++ test/ButtonSpec.js
@@ -26,21 +26,21 @@ describe('<Button>', () => {
       .should.equal('submit');
   });
 
-  it('Should not show the type if "as" is not an input', () => {
-    const wrapper = mount(
+  it('Should show the type if explicitly passed in when "as" is used', () => {
+    mount(
       <Button as="div" type="submit">
         Title
       </Button>,
-    );
-
-    expect(wrapper.getDOMNode().getAttribute('type')).to.be.null;
-  });
-
-  it('Should show the type if "as" is an input', () => {
-    mount(<Button as="input" type="submit" value="Title" />)
+    )
       .getDOMNode()
       .getAttribute('type')
       .should.equal('submit');
+  });
+
+  it('Should not have default type=button when "as" is used', () => {
+    const wrapper = mount(<Button as="div">Title</Button>);
+
+    expect(wrapper.getDOMNode().getAttribute('type')).to.be.null;
   });
 
   it('should forward refs to the button', () => {
