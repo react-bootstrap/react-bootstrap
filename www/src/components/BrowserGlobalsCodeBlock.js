06d5d02d56748de5b3774fe805c20543f19f423a
Sporego <contact@filbert.io>
2020-10-04 19:28:52 -0400

    Clarified the BrowserGlobalsCodeBlock in the GettingStardedPage with better comments and examples

--- www/src/components/BrowserGlobalsCodeBlock.js
+++ www/src/components/BrowserGlobalsCodeBlock.js
@@ -8,17 +8,23 @@ function BrowserGlobalsCodeBlock() {
     <CodeBlock
       mode="html"
       codeText={`
+/* This is React.js a React-Bootstrap depenancy... duhh*/ 
+/*Automatically added, if you are using a react framework (e.g., next.js, create-react-app, and gastby).*/
+
 <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
 
 <script
   src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
   crossorigin></script>
 
+/* This is React-Bootstrap's browser global*/
+/* Notice it is loaded after React.js*/
 <script
   src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
   crossorigin></script>
 
-<script>var Alert = ReactBootstrap.Alert;</script>
+/*Your other scripts*/
+<script>let ReactBootstrapBeAwesome = true </script>
 
 `}
     />
