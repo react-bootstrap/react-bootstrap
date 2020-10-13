6f6667ed32c67715008190059f86e938fe3be7ab
kyletsang <6854874+kyletsang@users.noreply.github.com>
2020-10-13 12:23:35 -0700

    Add active state to dark dropdown example to showcase highlight

--- www/src/examples/Dropdown/ButtonDark.js
+++ www/src/examples/Dropdown/ButtonDark.js
@@ -5,7 +5,9 @@
     </Dropdown.Toggle>
 
     <Dropdown.Menu variant="dark">
-      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+      <Dropdown.Item href="#/action-1" active>
+        Action
+      </Dropdown.Item>
       <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
       <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
       <Dropdown.Divider />
@@ -20,7 +22,9 @@
     title="Dropdown button"
     className="mt-2"
   >
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="#/action-1" active>
+      Action
+    </Dropdown.Item>
     <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
     <Dropdown.Divider />
