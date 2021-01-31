6fb97c2aaba1d8d6e1c43843738cf0719a536ac4
Shan Wong <shan.wong29@gmail.com>
2021-01-31 15:56:41 +0100

    fix: fix typo of alt in second slide of Carousel

--- tests/simple-types-test.tsx
+++ tests/simple-types-test.tsx
@@ -221,7 +221,7 @@ const MegaComponent = () => (
         <img
           className="d-block w-100"
           src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Third slide"
+          alt="Second slide"
         />
 
         <Carousel.Caption>
