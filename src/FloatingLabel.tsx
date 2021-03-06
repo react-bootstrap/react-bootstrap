0a11858420b08e011aa59587bb1afc073d52a2d1
kyletsang <6854874+kyletsang@users.noreply.github.com>
2021-03-05 17:13:30 -0800

    Clean up

--- src/FloatingLabel.tsx
+++ src/FloatingLabel.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React from 'react';
+import * as React from 'react';
 
 import FormGroup, { FormGroupProps } from './FormGroup';
 import { BsPrefixProps, BsPrefixRefForwardingComponent } from './helpers';
