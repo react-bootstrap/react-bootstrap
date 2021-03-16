793e857f852e5c799a12c203192eb3e1eeda7b33
Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
2021-03-16 10:28:14 -0700

    fix(Nav): add .card-header-* only if nested in card header (#5720)
    Rename CardContext to CardHeaderContext since it's only used for CardHeaders

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,13 +1,12 @@
 import classNames from 'classnames';
 import * as React from 'react';
-import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import { useBootstrapPrefix } from './ThemeProvider';
 import createWithBsPrefix from './createWithBsPrefix';
 import divWithClassName from './divWithClassName';
-import CardContext from './CardContext';
 import CardImg from './CardImg';
+import CardHeader from './CardHeader';
 import { BsPrefixProps, BsPrefixRefForwardingComponent } from './helpers';
 import { Color, Variant } from './types';
 
@@ -22,7 +21,6 @@ const CardSubtitle = createWithBsPrefix('card-subtitle', {
 });
 const CardLink = createWithBsPrefix('card-link', { Component: 'a' });
 const CardText = createWithBsPrefix('card-text', { Component: 'p' });
-const CardHeader = createWithBsPrefix('card-header');
 const CardFooter = createWithBsPrefix('card-footer');
 const CardImgOverlay = createWithBsPrefix('card-img-overlay');
 
@@ -95,29 +93,21 @@ const Card: BsPrefixRefForwardingComponent<'div', CardProps> = React.forwardRef<
     ref,
   ) => {
     const prefix = useBootstrapPrefix(bsPrefix, 'card');
-    const cardContext = useMemo(
-      () => ({
-        cardHeaderBsPrefix: `${prefix}-header`,
-      }),
-      [prefix],
-    );
 
     return (
-      <CardContext.Provider value={cardContext}>
-        <Component
-          ref={ref}
-          {...props}
-          className={classNames(
-            className,
-            prefix,
-            bg && `bg-${bg}`,
-            text && `text-${text}`,
-            border && `border-${border}`,
-          )}
-        >
-          {body ? <CardBody>{children}</CardBody> : children}
-        </Component>
-      </CardContext.Provider>
+      <Component
+        ref={ref}
+        {...props}
+        className={classNames(
+          className,
+          prefix,
+          bg && `bg-${bg}`,
+          text && `text-${text}`,
+          border && `border-${border}`,
+        )}
+      >
+        {body ? <CardBody>{children}</CardBody> : children}
+      </Component>
     );
   },
 );
