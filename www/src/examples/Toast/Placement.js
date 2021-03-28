04fde2f2d9cf3cf6513d84de845bba84ffee95cb
sohamsshah <sohamshah456@gmail.com>
2021-03-29 01:22:50 +0530

    update Toast Docs

--- www/src/examples/Toast/Placement.js
+++ www/src/examples/Toast/Placement.js
@@ -3,21 +3,23 @@
   aria-atomic="true"
   style={{
     position: 'relative',
-    minHeight: '100px',
+    minHeight: '200px',
   }}
 >
-  <Toast
+  <div
     style={{
       position: 'absolute',
       top: 0,
       right: 0,
     }}
   >
-    <Toast.Header>
-      <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-      <strong className="mr-auto">Bootstrap</strong>
-      <small>just now</small>
-    </Toast.Header>
-    <Toast.Body>See? Just like this.</Toast.Body>
-  </Toast>
+    <Toast>
+      <Toast.Header>
+        <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+        <strong className="mr-auto">Bootstrap</strong>
+        <small>2 seconds ago</small>
+      </Toast.Header>
+      <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
+    </Toast>
+  </div>
 </div>;
