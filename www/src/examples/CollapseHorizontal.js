86e19d6599bf06a54f619ec3e63014885a349e42
nsi319 <sairam.nareniyer@gmail.com>
2021-08-24 11:32:08 +0530

    feat: Added Horizontal Collapse

---
+++ www/src/examples/CollapseHorizontal.js
@@ -0,0 +1,28 @@
+function Example() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        click
+      </Button>
+      <div style={{minHeight: '150px'}}>
+        <Collapse in={open} dimension="width">
+          <div id="example-collapse-text">
+            <Card body style={{width: '400px'}}>
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                labore wes anderson cred nesciunt sapiente ea proident.
+            </Card>
+          </div>
+        </Collapse>
+      </div>
+    </>
+  );
+}
+
+render(<Example />);
