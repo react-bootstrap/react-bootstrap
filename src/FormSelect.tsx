4dd40db26df32fc5f98afdfd6c2f23ada920497a
Lawrence LCI <31262254+lcheunglci@users.noreply.github.com>
2021-10-28 22:02:30 -0700

    fix(FormSelect): fix props not being defined (#6106)
    * change HTMLAttributes to SelectHTMLAttributes
    
    * Omit size from interface since we use string instead of number to fix compile error

--- src/FormSelect.tsx
+++ src/FormSelect.tsx
@@ -8,7 +8,7 @@ import FormContext from './FormContext';
 
 export interface FormSelectProps
   extends BsPrefixOnlyProps,
-    React.HTMLAttributes<HTMLSelectElement> {
+    Omit<React.SelectHTMLAttributes<HTMLSelectElement>, 'size'> {
   htmlSize?: number;
   size?: 'sm' | 'lg';
   isValid?: boolean;
