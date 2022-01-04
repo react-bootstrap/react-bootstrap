1667a3d2cba7f006019e8db11e5dfdcf9634e671
Thorben Bochenek <bthorben@users.noreply.github.com>
2022-01-04 09:00:10 +0100

    Pass on click event to onHide for more options
    onHide is currently not passed the event. This means you cannot e.g. call event.stopPropagation() on the click event, preventing the event from bubbling up and calling actions on containers. Simply passing through the event seems safe and simple and gives more options (you could still ignore the event).

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -382,7 +382,7 @@ const Modal: BsPrefixRefForwardingComponent<'div', ModalProps> =
           return;
         }
 
-        onHide?.();
+        onHide?.(e);
       };
 
       const handleEscapeKeyDown = (e) => {
