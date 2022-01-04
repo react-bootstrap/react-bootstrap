616c03d881344e1ff76a78bd9287e3aea81d0f03
Szymon Wiszczuk <golota60@gmail.com>
2022-01-04 23:06:09 +0100

    test: move toast tests to RTL

---
+++ test/ToastHeaderSpec.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+
+import Toast from '../src/Toast';
+
+describe('Toast.Header', () => {
+  it('will pass all props to the created div and renders its children', () => {
+    const { container } = render(
+      <Toast.Header>
+        <strong>content</strong>
+      </Toast.Header>,
+    );
+    container.firstElementChild!.tagName === 'div';
+    container.firstElementChild!.firstElementChild!.tagName === 'strong';
+    container.firstElementChild!.classList.contains('toast-header').should.be
+      .true;
+  });
+
+  it('should render close button variant', () => {
+    const { container } = render(
+      <Toast.Header closeButton closeVariant="white">
+        <strong>content</strong>
+      </Toast.Header>,
+    );
+    container
+      .firstElementChild!.getElementsByTagName('button')[0]
+      .classList.contains('btn-close-white').should.be.true;
+  });
+});
