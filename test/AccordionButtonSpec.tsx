a8d282113e60d4a41d19c5831c82dbef140f25e1
VibhorJaiswal <vibhor.jaiswal9.9@gmail.com>
2022-01-09 07:02:55 +0000

    converted to rtl.

---
+++ test/AccordionButtonSpec.tsx
@@ -0,0 +1,30 @@
+import { fireEvent, render } from '@testing-library/react';
+import sinon from 'sinon';
+import AccordionButton from '../src/AccordionButton';
+
+describe('<AccordionButton>', () => {
+  it('Should have button as default component', () => {
+    const { getByText } = render(<AccordionButton />);
+    getByText('AccordionButton').tagName.toLowerCase().should.equal('button');
+    getByText('AccordionButton').type.should.equal('button');
+  });
+
+  it('Should allow rendering as different component', () => {
+    const { getByTestId } = render(
+      <AccordionButton data-testid="test-accordion-button" as="div" />,
+    );
+    getByTestId('test-accordion-button')
+      .tagName.toLowerCase()
+      .should.equal('div');
+  });
+
+  it('Should call onClick', () => {
+    const onClickSpy = sinon.spy();
+    const { getByTestId } = render(
+      <AccordionButton data-testid="btn" onClick={onClickSpy} />,
+    );
+    fireEvent.click(getByTestId('btn'));
+
+    onClickSpy.should.be.calledOnce;
+  });
+});
