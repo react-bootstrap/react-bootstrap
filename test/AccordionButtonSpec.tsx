86e063446f50a4fd7b3c24324d1b2e898c00d9dd
Vibhor Jaiswal <69683278+VibhorJaiswal@users.noreply.github.com>
2022-01-25 20:45:51 +0530

    Update AccordionButtonSpec.tsx
    minor changes

--- test/AccordionButtonSpec.tsx
+++ test/AccordionButtonSpec.tsx
@@ -4,9 +4,12 @@ import AccordionButton from '../src/AccordionButton';
 
 describe('<AccordionButton>', () => {
   it('Should have button as default component', () => {
-    const { getByTestId } = render(<AccordionButton data-testid="test-accordion-button" />);
-    getByTestId('test-accordion-button').tagName.toLowerCase().should.equal('button');
-    getByTestId('test-accordion-button').type.should.equal('button');
+    const { getByTestId } = render(
+      <AccordionButton data-testid="test-accordion-button" />,
+    );
+    getByTestId('test-accordion-button')
+      .tagName.toLowerCase()
+      .should.equal('button');
   });
 
   it('Should allow rendering as different component', () => {
