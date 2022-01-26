37a41aeb7401e0b50a8fa1be0d9c11bdd81084c8
Vibhor Jaiswal <69683278+VibhorJaiswal@users.noreply.github.com>
2022-01-27 03:24:40 +0530

    test: move AccordionButton to RTL. (#6180)
    * converted to rtl.
    
    * .
    
    * Update AccordionButtonSpec.tsx
    
    minor bug fixed.
    
    * Update AccordionButtonSpec.tsx
    
    minor changes
    
    * Create AccordionButtonSpec.tsx
    
    minor changes
    
    * Update AccordionButtonSpec.tsx
    
    minor changes

--- test/AccordionButtonSpec.tsx
+++ test/AccordionButtonSpec.tsx
@@ -1,14 +1,26 @@
-import { mount } from 'enzyme';
 import { fireEvent, render } from '@testing-library/react';
+import sinon from 'sinon';
 import AccordionButton from '../src/AccordionButton';
 
 describe('<AccordionButton>', () => {
   it('Should have button as default component', () => {
-    mount(<AccordionButton />).assertSingle('button[type="button"]');
+    const { getByTestId } = render(
+      <AccordionButton data-testid="test-accordion-button" />,
+    );
+    getByTestId('test-accordion-button')
+      .tagName.toLowerCase()
+      .should.equal('button');
+    getByTestId('test-accordion-button')
+      .type.should.equal('button');
   });
 
   it('Should allow rendering as different component', () => {
-    mount(<AccordionButton as="div" />).assertSingle('div.accordion-button');
+    const { getByTestId } = render(
+      <AccordionButton data-testid="test-accordion-button" as="div" />,
+    );
+    getByTestId('test-accordion-button')
+      .tagName.toLowerCase()
+      .should.equal('div');
   });
 
   it('Should call onClick', () => {
