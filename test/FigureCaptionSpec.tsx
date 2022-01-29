12ba0526b06ee29fe5675e1713df7181bf4661e7
Vibhor Jaiswal <69683278+VibhorJaiswal@users.noreply.github.com>
2022-01-29 19:04:08 +0530

    Convert to rtl.

---
+++ test/FigureCaptionSpec.tsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react';
+import Figure from '../src/Figure';
+
+describe('<Figure.Caption>', () => {
+  it('uses "figcaption" by default', () => {
+    const { getByTestId } = render(
+      <Figure >
+        <Figure.Caption data-testid="test-figure">Caption</Figure.Caption>
+      </Figure>
+    )
+    getByTestId('test-figure').tagName.toLowerCase().should.equal('figcaption');
+  });
+
+  it('has "figure-caption" class', () => {
+    const { getByTestId } = render(
+        <Figure.Caption data-testid="test-figure">Caption</Figure.Caption>
+    )
+    getByTestId('test-figure').classList.contains('figure-caption').should.be.true;
+  });
+
+  it('Should merge additional classes passed in', () => {
+    const { getByTestId } = render(
+      <Figure.Caption className="bob" data-testid="test-figure">Caption</Figure.Caption>
+    )
+    getByTestId('test-figure').classList.contains('bob').should.be.true;
+    getByTestId('test-figure').classList.contains('figure-caption').should.be.true;
+
+  });
+
+  it('allows custom elements instead of "figcaption"', () => {
+    const { getByTestId } = render(
+      <Figure.Caption as="section" data-testid="test-figure">Caption</Figure.Caption>
+    )
+    getByTestId('test-figure').tagName.toLowerCase().should.equal('section');
+  
+  });
+});
