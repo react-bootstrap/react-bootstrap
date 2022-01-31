56c4bca2b6c6ebf992365a5d1d9b228b3f3f673b
5f7d927fa2909600754134c1 <ahamed.althaf@freshworks.com>
2022-01-31 21:33:48 +0530

    Issue 6156 Added Tests for Modal component
    https://github.com/react-bootstrap/react-bootstrap/issues/6156
    
    Tests for Modal component

--- test/ModalSpec.tsx
+++ test/ModalSpec.tsx
@@ -1,18 +1,17 @@
-import { mount } from 'enzyme';
 import * as React from 'react';
+import { fireEvent, render, cleanup } from '@testing-library/react';
 import ModalManager from '@restart/ui/ModalManager';
-import Modal from '../src/Modal';
+import Modal, { ModalProps } from '../src/Modal';
 
 describe('<Modal>', () => {
   afterEach(() => {
-    // make sure the dangling portal elements get cleaned up
-    document.body.innerHTML = '';
+    cleanup();
   });
 
   it('Should forward ref to BaseModal', () => {
     const noOp = () => {};
-    const ref = React.createRef();
-    mount(
+    const ref = React.createRef<ModalProps>();
+    render(
       <Modal show onHide={noOp} animation={false} ref={ref}>
         <strong>Message</strong>
       </Modal>,
@@ -22,26 +21,24 @@ describe('<Modal>', () => {
 
   it('Should render the modal content', () => {
     const noOp = () => {};
-    mount(
-      <Modal show onHide={noOp} animation={false}>
+    const { getByTestId } = render(
+      <Modal show onHide={noOp} animation={false} data-testid="modal">
         <strong>Message</strong>
       </Modal>,
     )
-      .find('strong')
-      .text()
-      .should.equal('Message');
+    
+    expect(getByTestId('modal').querySelector('strong').textContent).to.equal('Message')
   });
 
   it('Should sets `display: block` to `div.modal` when animation is false', () => {
-    const node = mount(
-      <Modal show animation={false}>
+    const ref = React.createRef<ModalProps>();
+    const { getByTestId } = render(
+      <Modal show animation={false} ref={ref}>
         <strong>Message</strong>
       </Modal>,
     )
-      .find('div.modal')
-      .getDOMNode();
 
-    expect(node.style.display).to.equal('block');
+    expect(ref.current.dialog.style.display).to.equal('block');
   });
 
   it('Should close the modal when the modal dialog is clicked', (done) => {
@@ -49,45 +46,45 @@ describe('<Modal>', () => {
       done();
     };
 
-    mount(
+    const { getByRole } = render(
       <Modal show onHide={doneOp}>
         <strong>Message</strong>
       </Modal>,
     )
-      .find('div.modal') // the modal-dialog element is pointer-events: none;
-      .simulate('click');
+
+    // the modal-dialog element is pointer-events: none;
+    fireEvent.click(getByRole('dialog'));
+    expect(onHideSpy).to.have.been.called;
   });
 
   it('Should not close the modal when the "static" dialog is clicked', () => {
     const onHideSpy = sinon.spy();
-    mount(
-      <Modal show onHide={onHideSpy} backdrop="static">
+    const { getByTestId } = render(
+      <Modal show onHide={onHideSpy} backdrop="static" data-testid="modal">
         <strong>Message</strong>
       </Modal>,
     )
-      .find('ModalDialog')
-      .simulate('click');
 
+    fireEvent.click(getByTestId('modal'));
     expect(onHideSpy).to.not.have.been.called;
   });
 
   it('Should show "static" dialog animation when backdrop is clicked', () => {
     const noOp = () => {};
-    const wrapper = mount(
+    const { getByRole } = render(
       <Modal show onHide={noOp} backdrop="static">
         <strong>Message</strong>
       </Modal>,
     );
 
-    const modal = wrapper.find('.modal');
-    modal.simulate('click');
-
-    expect(wrapper.find('.modal-static').length).to.equal(1);
+    const modalDialog = getByRole('dialog');
+    fireEvent.click(modalDialog);
+    getByRole('dialog').classList.contains('modal-static').should.be.true;
   });
 
   it('Should show "static" dialog animation when esc pressed and keyboard is false', () => {
     const noOp = () => {};
-    const wrapper = mount(
+    const { getByRole } = render(
       <Modal show onHide={noOp} backdrop="static" keyboard={false}>
         <strong>Message</strong>
       </Modal>,
@@ -95,14 +92,12 @@ describe('<Modal>', () => {
 
     const event = new KeyboardEvent('keydown', { keyCode: 27 });
     document.dispatchEvent(event);
-    wrapper.update();
-
-    expect(wrapper.find('.modal-static').length).to.equal(1);
+    getByRole('dialog').classList.contains('modal-static').should.be.true;
   });
 
   it('Should not show "static" dialog animation when esc pressed and keyboard is true', () => {
     const noOp = () => {};
-    const wrapper = mount(
+    const { getByRole } = render(
       <Modal show onHide={noOp} backdrop="static" keyboard>
         <strong>Message</strong>
       </Modal>,
@@ -110,23 +105,19 @@ describe('<Modal>', () => {
 
     const event = new KeyboardEvent('keydown', { keyCode: 27 });
     document.dispatchEvent(event);
-    wrapper.update();
-
-    expect(wrapper.find('.modal-static').length).to.equal(0);
+    getByRole('dialog').classList.contains('modal-static').should.be.false;
   });
 
   it('Should not show "static" dialog animation modal backdrop is not "static"', () => {
     const noOp = () => {};
-    const wrapper = mount(
-      <Modal show onHide={noOp} backdrop>
+    const { getByTestId, getByRole } = render(
+      <Modal show onHide={noOp} backdrop data-testid="modal">
         <strong>Message</strong>
       </Modal>,
     );
 
-    const modal = wrapper.find('.modal');
-    modal.simulate('click');
-
-    expect(wrapper.find('.modal-static').length).to.equal(0);
+    fireEvent.click(getByTestId('modal'));
+    getByRole('dialog').classList.contains('modal-static').should.be.false;
   });
 
   it('Should close the modal when the modal close button is clicked', (done) => {
@@ -134,107 +125,124 @@ describe('<Modal>', () => {
       done();
     };
 
-    mount(
+    const { getByTestId } = render(
       <Modal show onHide={doneOp}>
-        <Modal.Header closeButton />
+        <Modal.Header closeButton data-testid="close-btn"/>
         <strong>Message</strong>
       </Modal>,
     )
-      .find('.btn-close')
-      .simulate('click');
+
+    fireEvent.click(getByTestId('close-btn').querySelector('button'));
   });
 
   it('Should pass className to the dialog', () => {
     const noOp = () => {};
-    mount(
+    const { getByRole } = render(
       <Modal show className="mymodal" onHide={noOp}>
         <strong>Message</strong>
       </Modal>,
-    ).assertSingle('div.modal.mymodal');
+    );
+
+    getByRole('dialog').classList.contains('mymodal').should.be.true;
   });
 
   it('Should use backdropClassName to add classes to the backdrop', () => {
     const noOp = () => {};
 
-    mount(
+    render(
       <Modal show backdropClassName="my-modal-backdrop" onHide={noOp}>
         <strong>Message</strong>
       </Modal>,
-    ).find('.modal-backdrop.my-modal-backdrop');
+    );
+
+    document.querySelector('.modal-backdrop').classList.contains('my-modal-backdrop').should.be.true;
   });
 
   it('Should pass size to the dialog', () => {
     const noOp = () => {};
-    mount(
-      <Modal show size="sm" onHide={noOp}>
+    const { getByTestId } = render(
+      <Modal show size="sm" onHide={noOp} data-testid="modal">
         <strong>Message</strong>
       </Modal>,
-    ).find('.modal-dialog.modal-sm');
+    );
+
+    getByTestId('modal').classList.contains('modal-sm').should.be.true;
   });
 
   it('Should pass fullscreen as bool to the dialog', () => {
-    mount(
-      <Modal show fullscreen>
+    const { getByTestId } = render(
+      <Modal show fullscreen data-testid="modal">
         <strong>Message</strong>
       </Modal>,
-    ).assertSingle('.modal-dialog.modal-fullscreen');
+    )
+
+    getByTestId('modal').classList.contains('modal-fullscreen').should.be.true;
   });
 
   it('Should pass fullscreen as string to the dialog', () => {
-    mount(
-      <Modal show fullscreen="sm-down">
+    const { getByTestId } = render(
+      <Modal show fullscreen="sm-down" data-testid="modal">
         <strong>Message</strong>
       </Modal>,
-    ).assertSingle('.modal-dialog.modal-fullscreen-sm-down');
+    )
+
+    getByTestId('modal').classList.contains('modal-fullscreen-sm-down').should.be.true;
   });
 
   it('Should pass centered to the dialog', () => {
     const noOp = () => {};
-    mount(
-      <Modal show centered onHide={noOp}>
+    const { getByTestId } = render(
+      <Modal show centered onHide={noOp} data-testid="modal">
         <strong>Message</strong>
       </Modal>,
-    ).find('.modal-dialog.modal-dialog-centered');
+    )
+
+    getByTestId('modal').classList.contains('modal-dialog-centered').should.be.true;
   });
 
   it('Should pass scrollable to the dialog', () => {
     const noOp = () => {};
-    mount(
-      <Modal show scrollable onHide={noOp}>
+    const { getByTestId } = render(
+      <Modal show scrollable onHide={noOp} data-testid="modal">
         <strong>Message</strong>
       </Modal>,
-    ).find('.modal-dialog.modal-dialog-scrollable');
+    )
+
+    getByTestId('modal').classList.contains('modal-dialog-scrollable').should.be.true;
   });
 
   it('Should pass dialog style to the dialog', () => {
     const noOp = () => {};
-    const dialog = mount(
+    const { getByRole } = render(
       <Modal show style={{ color: 'red' }} onHide={noOp}>
         <strong>Message</strong>
       </Modal>,
     )
-      .find('div.modal')
-      .getDOMNode();
 
-    assert.ok(dialog.style.color === 'red');
+    assert.ok(getByRole('dialog').style.color === 'red');
   });
 
   it('Should pass dialogClassName to the dialog', () => {
     const noOp = () => {};
-    mount(
-      <Modal show dialogClassName="my-dialog" onHide={noOp}>
+    const { getByTestId } = render(
+      <Modal show dialogClassName="my-dialog" onHide={noOp} data-testid="modal">
         <strong>Message</strong>
       </Modal>,
-    ).assertSingle('.modal-dialog.my-dialog');
+    )
+
+    getByTestId('modal').classList.contains('my-dialog').should.be.true;
   });
 
   it('Should pass contentClassName to .modal-content', () => {
     const noOp = () => {};
-    mount(
-      <Modal show contentClassName="my-content" onHide={noOp}>
+    const { getByTestId } = render(
+      <Modal show contentClassName="my-content" onHide={noOp} data-testid="modal">
         <strong>Message</strong>
       </Modal>,
-    ).assertSingle('.modal-content.my-content');
+    );
+
+    const modalContent = getByTestId('modal').querySelector('.modal-content');
+    modalContent.classList.contains('my-content').should.be.true;
   });
 
   it('Should use dialogAs', () => {
@@ -244,59 +252,71 @@ describe('<Modal>', () => {
       return <div className="custom-dialog" tabIndex="-1" />;
     }
 
-    mount(
+    render(
       <Modal show dialogAs={CustomDialog} onHide={noOp}>
         <strong>Message</strong>
       </Modal>,
-    ).assertSingle('.custom-dialog');
+    );
+
+    document.querySelector('.custom-dialog').should.exist;
   });
 
   it('Should pass transition callbacks to Transition', (done) => {
     const increment = sinon.spy();
+    const Elem = () => {
+      const [show, setShow] = React.useState(true);
+      return (
+        <Modal
+          show={show}
+          onHide={() => {}}
+          onEnter={increment}
+          onEntering={increment}
+          onEntered={() => {
+            increment();
+            setShow(false);
+          }}
+          onExit={increment}
+          onExiting={increment}
+          onExited={() => {
+            increment();
+            expect(increment.callCount).to.equal(6);
+            done();
+          }}
+        >
+          <strong>Message</strong>
+        </Modal>
+      );
+    };
 
-    const instance = mount(
-      <Modal
-        show
-        onHide={() => {}}
-        onExit={increment}
-        onExiting={increment}
-        onExited={() => {
-          increment();
-          expect(increment.callCount).to.equal(6);
-          done();
-        }}
-        onEnter={increment}
-        onEntering={increment}
-        onEntered={() => {
-          increment();
-          instance.setProps({ show: false });
-        }}
-      >
-        <strong>Message</strong>
-      </Modal>,
-    );
+    render(<Elem />);
   });
 
   it('should call `transitionend` before `exited`', (done) => {
     const increment = sinon.spy();
     let modal;
 
-    const instance = mount(
+    const { getByTestId, getByRole, rerender } = render(
       <Modal
         show
+        data-testid="modal"
         style={{ transition: 'opacity 1s linear' }}
+        onHide={() => {}}
+      >
+        <strong>Message</strong>
+      </Modal>,
+    );
+    modal = getByRole('dialog');
+    modal.addEventListener('transitionend', increment);
+    rerender(
+      <Modal
+        show={false}
         onExited={() => {
           expect(increment.callCount).to.equal(1);
           modal.removeEventListener('transitionend', increment);
           done();
         }}
-      >
-        <strong>Message</strong>
-      </Modal>,
+      >Foo</Modal>
     );
-    modal = instance.find('.modal').getDOMNode();
-    modal.addEventListener('transitionend', increment);
-    instance.setProps({ show: false });
   });
 
   describe('cleanup', () => {
@@ -325,8 +345,8 @@ describe('<Modal>', () => {
         }
       }
 
-      const instance = mount(<Component />);
-      instance.setState({ show: false });
+      const { rerender } = render(<Component />);
+      rerender(<Modal show={false}>Foo</Modal>)
 
       expect(offSpy).to.have.been.calledWith('resize');
     });
@@ -334,50 +354,48 @@ describe('<Modal>', () => {
 
   it('Should close once it was clicked outside of the Modal', () => {
     const onHideSpy = sinon.spy();
-    mount(
+    const { getByRole } = render(
       <Modal show onHide={onHideSpy}>
         <strong>Message</strong>
       </Modal>,
     )
-      .find('div.modal.show')
-      .simulate('click');
-
+    
+    fireEvent.click(getByRole('dialog'));
     expect(onHideSpy).to.have.been.called;
   });
 
   it('Should not call onHide if the click target comes from inside the dialog', () => {
     const onHideSpy = sinon.spy();
-    const wrapper = mount(
-      <Modal show onHide={onHideSpy}>
+    const { getByTestId, getByRole } = render(
+      <Modal show onHide={onHideSpy} data-testid="modal">
         <strong>Message</strong>
       </Modal>,
     );
 
-    wrapper.find('div.modal-dialog').simulate('mouseDown');
-    wrapper.find('div.modal.show').simulate('mouseUp');
-    wrapper.find('div.modal.show').simulate('click');
+    fireEvent.mouseDown(getByTestId('modal'));
+    fireEvent.mouseUp(getByRole('dialog'));
+    fireEvent.click(getByRole('dialog'));
 
     expect(onHideSpy).to.not.have.been.called;
   });
 
   it('Should set aria-labelledby to the role="dialog" element if aria-labelledby set', () => {
     const noOp = () => {};
-    const wrapper = mount(
+    const { getByRole } = render(
       <Modal show onHide={noOp} aria-labelledby="modal-title">
         <Modal.Header closeButton>
           <Modal.Title id="modal-title">Modal heading</Modal.Title>
         </Modal.Header>
       </Modal>,
     );
-    wrapper.assertSingle(
-      'div.modal.show[role="dialog"][aria-labelledby="modal-title"]',
-    );
+
+    expect(getByRole('dialog').getAttribute('aria-labelledby')).to.equal('modal-title');
   });
 
   it('Should call onEscapeKeyDown when keyboard is true', () => {
     const noOp = () => {};
     const onEscapeKeyDownSpy = sinon.spy();
-    mount(
+    render(
       <Modal show onHide={noOp} keyboard onEscapeKeyDown={onEscapeKeyDownSpy}>
         <strong>Message</strong>
       </Modal>,
@@ -392,7 +410,7 @@ describe('<Modal>', () => {
   it('Should not call onEscapeKeyDown when keyboard is false', () => {
     const noOp = () => {};
     const onEscapeKeyDownSpy = sinon.spy();
-    mount(
+    render(
       <Modal
         show
         onHide={noOp}
@@ -418,10 +436,10 @@ describe('<Modal>', () => {
       }
     }
 
-    const managerRef = React.createRef();
+    const managerRef = React.createRef<ModalProps>();
     managerRef.current = new MyModalManager();
 
-    mount(
+    render(
       <Modal show onHide={noOp} manager={managerRef.current}>
         <strong>Message</strong>
       </Modal>,
