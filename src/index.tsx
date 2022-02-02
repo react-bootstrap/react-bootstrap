9ff63bc5dc0664038b6a3aafd6a745565fe11179
VibhorJaiswal <vibhor.jaiswal9.9@gmail.com>
2022-02-02 15:19:48 +0000

    added some files.

--- src/index.tsx
+++ src/index.tsx
@@ -112,8 +112,13 @@ export { default as Modal } from './Modal';
 export type { ModalProps } from './Modal';
 
 export { default as ModalBody } from './ModalBody';
+export { default as ModalContext } from './ModalContext';
+
 export { default as ModalDialog } from './ModalDialog';
 export type { ModalDialogProps } from './ModalDialog';
+
+export { default as ModalHeader } from './ModalHeader';
+export type { ModalHeaderProps } from './ModalHeader';
 
 export { default as ModalFooter } from './ModalFooter';
 export { default as ModalTitle } from './ModalTitle';
