ad2b41b3ae0e401d80343aec1c9a20b5329c8110
kyletsang <6854874+kyletsang@users.noreply.github.com>
2022-04-25 12:50:42 -0700

    docs: fix examples

--- www/src/examples/Alert/Basic.js
+++ www/src/examples/Alert/Basic.js
@@ -1,14 +1,16 @@
-[
-  'primary',
-  'secondary',
-  'success',
-  'danger',
-  'warning',
-  'info',
-  'light',
-  'dark',
-].map((variant, idx) => (
-  <Alert key={idx} variant={variant}>
-    This is a {variant} alert—check it out!
-  </Alert>
-));
+<>
+  {[
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+  ].map((variant) => (
+    <Alert key={variant} variant={variant}>
+      This is a {variant} alert—check it out!
+    </Alert>
+  ))}
+</>;
