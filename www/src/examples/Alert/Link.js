ff08942c28b8a7a16cb4fc14787c5864bd0bbe95
Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
2022-04-25 18:41:13 -0700

    docs: fix examples (#6311)
    * docs: fix examples
    
    * docs: fix more examples

--- www/src/examples/Alert/Link.js
+++ www/src/examples/Alert/Link.js
@@ -1,16 +1,18 @@
-[
-  'primary',
-  'secondary',
-  'success',
-  'danger',
-  'warning',
-  'info',
-  'light',
-  'dark',
-].map((variant, idx) => (
-  <Alert key={idx} variant={variant}>
-    This is a {variant} alert with{' '}
-    <Alert.Link href="#">an example link</Alert.Link>. Give it a click if you
-    like.
-  </Alert>
-));
+<>
+  {[
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+  ].map((variant) => (
+    <Alert key={variant} variant={variant}>
+      This is a {variant} alert with{' '}
+      <Alert.Link href="#">an example link</Alert.Link>. Give it a click if you
+      like.
+    </Alert>
+  ))}
+</>;
--- www/src/examples/ListGroup/HorizontalResponsive.js
+++ www/src/examples/ListGroup/HorizontalResponsive.js
@@ -1,8 +1,10 @@
-['sm', 'md', 'lg', 'xl', 'xxl'].map((breakpoint, idx) => (
-  <ListGroup horizontal={breakpoint} className="my-2" key={idx}>
-    <ListGroup.Item>This ListGroup</ListGroup.Item>
-    <ListGroup.Item>renders horizontally</ListGroup.Item>
-    <ListGroup.Item>on {breakpoint}</ListGroup.Item>
-    <ListGroup.Item>and above!</ListGroup.Item>
-  </ListGroup>
-));
+<>
+  {['sm', 'md', 'lg', 'xl', 'xxl'].map((breakpoint) => (
+    <ListGroup key={breakpoint} horizontal={breakpoint} className="my-2">
+      <ListGroup.Item>This ListGroup</ListGroup.Item>
+      <ListGroup.Item>renders horizontally</ListGroup.Item>
+      <ListGroup.Item>on {breakpoint}</ListGroup.Item>
+      <ListGroup.Item>and above!</ListGroup.Item>
+    </ListGroup>
+  ))}
+</>;
