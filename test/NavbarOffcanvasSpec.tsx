63869f923995d3897c3e4f3d77433cf8a3f6505e
Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
2022-04-27 12:16:11 -0700

    fix(NavbarOffcanvas): fix rendering of nav items when expand is used (#6310)

--- test/NavbarOffcanvasSpec.tsx
+++ test/NavbarOffcanvasSpec.tsx
@@ -31,4 +31,15 @@ describe('<NavbarOffcanvas>', () => {
     fireEvent.click(getByLabelText('Close'));
     onToggleSpy.should.have.been.calledWith(false);
   });
+
+  it('should render nav items with expand prop', () => {
+    const { getByText } = render(
+      <Navbar expand="sm">
+        <Navbar.Toggle data-testid="toggle" />
+        <Navbar.Offcanvas data-testid="offcanvas">hello</Navbar.Offcanvas>
+      </Navbar>,
+    );
+
+    getByText('hello').should.exist;
+  });
 });
