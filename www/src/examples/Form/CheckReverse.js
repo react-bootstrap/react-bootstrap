dd4191190b118840351f5cd8e20ccc7add3e98a8
andydetorres <66095650+andydetorres@users.noreply.github.com>
2022-05-24 21:40:41 -0700

    feat(FormCheck): add support for reverse checks and radios (#6353)

---
+++ www/src/examples/Form/CheckReverse.js
@@ -0,0 +1,27 @@
+<Form>
+  {['checkbox', 'radio'].map((type) => (
+    <div key={`reverse-${type}`} className="mb-3">
+      <Form.Check
+        reverse
+        label="1"
+        name="group1"
+        type={type}
+        id={`reverse-${type}-1`}
+      />
+      <Form.Check
+        reverse
+        label="2"
+        name="group1"
+        type={type}
+        id={`reverse-${type}-2`}
+      />
+      <Form.Check
+        reverse
+        disabled
+        label="3 (disabled)"
+        type={type}
+        id={`reverse-${type}-3`}
+      />
+    </div>
+  ))}
+</Form>;
