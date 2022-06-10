8fe2a316ff75f688ae84013443e7aa7ed2199d84
Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
2022-06-09 17:27:14 -0700

    chore: fix eslint for docs (#6374)

--- www/src/components/CarbonAds.js
+++ www/src/components/CarbonAds.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 const CarbonAds = () => {
   const ref = useRef();
--- www/src/examples/AsProp.js
+++ www/src/examples/AsProp.js
@@ -1,5 +1,11 @@
 <Row className="mx-0">
-  <Button as={Col} variant="primary">Button #1</Button>
-  <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
-  <Button as={Col} variant="success">Button #3</Button>
+  <Button as={Col} variant="primary">
+    Button #1
+  </Button>
+  <Button as={Col} variant="secondary" className="mx-2">
+    Button #2
+  </Button>
+  <Button as={Col} variant="success">
+    Button #3
+  </Button>
 </Row>;
