f8e41232388d250d3447403b4f8861557b4af81c
JustinTime42 <justincase18@msn.com>
2022-07-06 13:19:38 -0800

    Revert "added a note in the documentation for OverlayTrigger that the click trigger ignores the configured delay"
    This reverts commit aeea7df4d89131d2f11e5a705af670e723030a9d.

--- src/OverlayTrigger.tsx
+++ src/OverlayTrigger.tsx
@@ -74,8 +74,6 @@ const propTypes = {
   /**
    * Specify which action or actions trigger Overlay visibility
    *
-   * The `click` trigger ignores the configured `delay`.
-   *
    * @type {'hover' | 'click' |'focus' | Array<'hover' | 'click' |'focus'>}
    */
   trigger: PropTypes.oneOfType([triggerType, PropTypes.arrayOf(triggerType)]),
