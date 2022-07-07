af953c20361d91a50c06b75c8bc7d6bb13da3165
Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
2022-07-06 17:04:14 -0700

    docs: fix custom tabs example (#6396)

--- www/src/examples/Tabs/LeftTabs.js
+++ www/src/examples/Tabs/LeftTabs.js
@@ -12,10 +12,14 @@ function LeftTabsExample() {
         <Col sm={3}>
           <Nav variant="pills" className="flex-column">
             <Nav.Item>
-              <Nav.Link eventKey="first">Tab 1</Nav.Link>
+              <Nav.Link eventKey="first" href="#">
+                Tab 1
+              </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+              <Nav.Link eventKey="second" href="#">
+                Tab 2
+              </Nav.Link>
             </Nav.Item>
           </Nav>
         </Col>
