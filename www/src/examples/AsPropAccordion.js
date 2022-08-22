7aa416164ae5c5e862e5e593de3d600122dea496
Shannon <shannondmello@gmail.com>
2022-08-22 14:09:05 +0530

    Docs: Added Example For React Bootstarp Component in As Prop API

---
+++ www/src/examples/AsPropAccordion.js
@@ -0,0 +1,14 @@
+<div>
+  <h1>
+    Example heading{' '}
+    <Badge bg="secondary" as="Button">
+      New
+    </Badge>
+  </h1>
+  <h2>
+    Example heading{' '}
+    <Button bg="secondary" as="Badge">
+      New
+    </Button>
+  </h2>
+</div>;
