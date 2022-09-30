732b9889e267849be74567dd709f848954bdc7d1
Ayuhito <hello@ayuhito.com>
2022-10-01 00:06:32 +0100

    docs: switch default cdn

--- www/src/components/BrowserGlobalsCodeBlock.js
+++ www/src/components/BrowserGlobalsCodeBlock.js
@@ -7,14 +7,14 @@ function BrowserGlobalsCodeBlock() {
     <CodeBlock
       mode="html"
       codeText={`
-<script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+<script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
 
 <script
-  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+  src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
   crossorigin></script>
 
 <script
-  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+  src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
   crossorigin></script>
 
 <script>var Alert = ReactBootstrap.Alert;</script>
