f2a91ed87925b4861d0e31a8074b76eec5d85889
kyletsang <6854874+kyletsang@users.noreply.github.com>
2022-11-24 21:31:10 -0800

    docs(Modal): fix static example

--- www/src/examples/Modal/Static.js
+++ www/src/examples/Modal/Static.js
@@ -3,20 +3,25 @@ import Modal from 'react-bootstrap/Modal';
 
 function StaticExample() {
   return (
-    <Modal.Dialog>
-      <Modal.Header closeButton>
-        <Modal.Title>Modal title</Modal.Title>
-      </Modal.Header>
+    <div
+      className="modal show"
+      style={{ display: 'block', position: 'initial' }}
+    >
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
 
-      <Modal.Body>
-        <p>Modal body text goes here.</p>
-      </Modal.Body>
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
 
-      <Modal.Footer>
-        <Button variant="secondary">Close</Button>
-        <Button variant="primary">Save changes</Button>
-      </Modal.Footer>
-    </Modal.Dialog>
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
   );
 }
 
