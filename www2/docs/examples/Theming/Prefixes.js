f4e488d082d5e02abc9a24b2f8e8d3a31e1043cd
kyletsang <6854874+kyletsang@users.noreply.github.com>
2023-02-16 23:19:47 -0800

    wip

--- www2/docs/examples/Theming/Prefixes.js
+++ www2/docs/examples/Theming/Prefixes.js
@@ -7,7 +7,7 @@ function PrefixesExample() {
       {/* Hint: inspect the markup to see how the classes differ */}
       <ThemeProvider prefixes={{ btn: 'my-btn' }}>
         <Button variant="primary">My Button</Button>
-      </ThemeProvider>{' '}
+      </ThemeProvider>
       <Button bsPrefix="super-btn" variant="primary">
         Super button
       </Button>
