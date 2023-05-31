e146ffbfa996c81aee77a40004e547ff09cb09a3
Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
2023-05-30 21:35:51 -0700

    docs: fix deployment project (#6620)
    * docs: fix deployment project
    
    * branch

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -18,7 +18,8 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'react-bootstrap', // Usually your GitHub org/user name.
-  projectName: 'react-bootstrap', // Usually your repo name.
+  projectName: 'react-bootstrap.github.io', // Usually your repo name.
+  deploymentBranch: 'master',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
