986d3a0e5cc909e1b88b41fcd89c7a27e0d0fb46
kyletsang <6854874+kyletsang@users.noreply.github.com>
2023-06-01 17:22:42 -0700

    fix(Tooltip): fix offset

--- src/useOverlayOffset.tsx
+++ src/useOverlayOffset.tsx
@@ -3,31 +3,40 @@ import hasClass from 'dom-helpers/hasClass';
 import { Offset, Options } from '@restart/ui/usePopper';
 import { useBootstrapPrefix } from './ThemeProvider';
 import Popover from './Popover';
+import Tooltip from './Tooltip';
 
 // This is meant for internal use.
-// This applies a custom offset to the overlay if it's a popover.
+// This applies a custom offset to the overlay if it's a popover or tooltip.
 export default function useOverlayOffset(
   customOffset?: Offset,
 ): [React.RefObject<HTMLElement>, Options['modifiers']] {
   const overlayRef = useRef<HTMLDivElement | null>(null);
   const popoverClass = useBootstrapPrefix(undefined, 'popover');
+  const tooltipClass = useBootstrapPrefix(undefined, 'tooltip');
 
   const offset = useMemo(
     () => ({
       name: 'offset',
       options: {
         offset: () => {
-          if (
-            overlayRef.current &&
-            hasClass(overlayRef.current, popoverClass)
-          ) {
-            return customOffset || Popover.POPPER_OFFSET;
+          if (customOffset) {
+            return customOffset;
           }
-          return customOffset || [0, 0];
+
+          if (overlayRef.current) {
+            if (hasClass(overlayRef.current, popoverClass)) {
+              return Popover.POPPER_OFFSET;
+            }
+
+            if (hasClass(overlayRef.current, tooltipClass)) {
+              return Tooltip.TOOLTIP_OFFSET;
+            }
+          }
+          return [0, 0];
         },
       },
     }),
-    [customOffset, popoverClass],
+    [customOffset, popoverClass, tooltipClass],
   );
 
   return [overlayRef, [offset]];
