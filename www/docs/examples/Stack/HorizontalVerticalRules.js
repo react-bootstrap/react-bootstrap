0c45a1ad61fddf4cd41a6d70a4e1e4144d67eae9
kyletsang <6854874+kyletsang@users.noreply.github.com>
2023-06-11 23:10:57 -0700

    fix stack examples

--- www/docs/examples/Stack/HorizontalVerticalRules.js
+++ www/docs/examples/Stack/HorizontalVerticalRules.js
@@ -3,10 +3,10 @@ import Stack from 'react-bootstrap/Stack';
 function HorizontalVerticalRulesExample() {
   return (
     <Stack direction="horizontal" gap={3}>
-      <div className="bg-warning border">First item</div>
-      <div className="bg-warning border ms-auto">Second item</div>
+      <div className="p-2">First item</div>
+      <div className="p-2 ms-auto">Second item</div>
       <div className="vr" />
-      <div className="bg-warning border">Third item</div>
+      <div className="p-2">Third item</div>
     </Stack>
   );
 }
