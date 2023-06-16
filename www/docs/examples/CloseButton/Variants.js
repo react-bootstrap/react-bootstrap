eb8788cfcc057972a88aeefbcde9748e4ed20a3f
kyletsang <6854874+kyletsang@users.noreply.github.com>
2023-06-15 22:25:00 -0700

    address feedback

--- www/docs/examples/CloseButton/Variants.js
+++ www/docs/examples/CloseButton/Variants.js
@@ -2,7 +2,7 @@ import CloseButton from 'react-bootstrap/CloseButton';
 
 function VariantsExample() {
   return (
-    <div data-bs-theme="dark">
+    <div data-bs-theme="dark" className='bg-dark p-2'>
       <CloseButton />
       <CloseButton disabled />
     </div>
