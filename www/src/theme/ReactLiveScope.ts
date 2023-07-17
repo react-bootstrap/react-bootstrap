0f0a14785af8d809dd5823b48622d30a4ae395ad
Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
2023-07-16 22:46:45 -0700

    docs: fix carousel example colors (#6662)

--- www/src/theme/ReactLiveScope.ts
+++ www/src/theme/ReactLiveScope.ts
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactBootstrap from 'react-bootstrap';
 import * as formik from 'formik';
 import * as yup from 'yup';
+import ExampleCarouselImage from '../components/ExampleCarouselImage';
 
 // Add react-live imports you need here
 const ReactLiveScope = {
@@ -10,6 +11,7 @@ const ReactLiveScope = {
   ...ReactBootstrap,
   formik,
   yup,
+  ExampleCarouselImage,
 };
 
 delete (ReactLiveScope as any).default;
