ba6d8b509471a0412688a26cf66fd7e45073afb0
asamad35 <samad.abdus3535@gmail.com>
2023-08-02 00:08:55 +0530

    fixed broken navbar in /docs/components/navbar directory

--- www/docs/examples/Navbar/Basic.js
+++ www/docs/examples/Navbar/Basic.js
@@ -5,7 +5,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary h-auto">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
