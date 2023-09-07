7a02a4bdb11d7b8f3f9d23a5dcd471a163435f56
Gary Hagen <HagenGaryP@gmail.com>
2023-09-06 20:40:17 -0400

    docs(NavDropdown): correct minor spelling error in example (#6689)

--- www/docs/examples/Navbar/Collapsible.js
+++ www/docs/examples/Navbar/Collapsible.js
@@ -13,7 +13,7 @@ function CollapsibleExample() {
           <Nav className="me-auto">
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
