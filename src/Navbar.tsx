b00e9d069b1cac9637944ecd1b3b015555faf393
TJB <tjb042@users.noreply.github.com>
2024-04-05 20:02:28 -0400

    Update Navbar.tsx

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -109,8 +109,8 @@ const propTypes = {
    * Toggles `expanded` to `false` after the onSelect event of a descendant of a
    * child `<Nav>` fires. Does nothing if no `<Nav>` or `<Nav>` descendants exist.
    *
-   * `<Nav.Link>` descentants of `<Nav>` will not trigger the onSelect event unless
-   * the `eventKey` or `href` props are defined.
+   * `<NavLink>` descentants of `<Nav>` will not trigger the onSelect event unless
+   * an `eventKey` or `href` prop is defined.
    *
    * Manually controlling `expanded` via the onSelect callback is recommended instead,
    * for more complex operations that need to be executed after
