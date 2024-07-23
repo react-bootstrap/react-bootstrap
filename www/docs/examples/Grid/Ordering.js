0952140e2fb118537410c819d15eaedb423cdc0f
Pablo <61226966+zarzas980@users.noreply.github.com>
2024-07-23 08:23:56 +0200

    docs: fix mistake with grid example (#6826)

--- www/docs/examples/Grid/Ordering.js
+++ www/docs/examples/Grid/Ordering.js
@@ -7,8 +7,8 @@ function OrderingExample() {
     <Container>
       <Row>
         <Col xs>First, but unordered</Col>
-        <Col xs={{ order: 12 }}>Second, but last</Col>
-        <Col xs={{ order: 1 }}>Third, but second</Col>
+        <Col xs={{ order: 5 }}>Second, but last</Col>
+        <Col xs={{ order: 0 }}>Third, but second</Col>
       </Row>
     </Container>
   );
