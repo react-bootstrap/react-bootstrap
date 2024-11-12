6c527f7ae2f2056a9705ff1e34694c3d3cbcbeeb
Anusha Chaturvedi <anusha.chaturvedi@canonical.com>
2024-11-12 15:55:39 +0530

    docs: fix link in breadcrumb

--- www/docs/examples/Breadcrumb.js
+++ www/docs/examples/Breadcrumb.js
@@ -4,7 +4,7 @@ function BreadcrumbExample() {
   return (
     <Breadcrumb>
       <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-      <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+      <Breadcrumb.Item href="#">
         Library
       </Breadcrumb.Item>
       <Breadcrumb.Item active>Data</Breadcrumb.Item>
