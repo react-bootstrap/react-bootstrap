b2b348d5039ae9cc6f431db5f74e8e419c0496a9
VibhorJaiswal <vibhor.jaiswal9.9@gmail.com>
2024-11-15 19:53:43 +0530

    improved as per suggestions

--- www/src/theme/Playground/Preview.tsx
+++ www/src/theme/Playground/Preview.tsx
@@ -4,6 +4,7 @@ import { LiveContext, LivePreview } from 'react-live';
 import qsa from 'dom-helpers/querySelectorAll';
 import useIsomorphicEffect from '@restart/hooks/useIsomorphicEffect';
 import useMutationObserver from '@restart/hooks/useMutationObserver';
+import { useEventCallback } from '@restart/hooks';
 
 export interface PreviewProps {
   className?: string | undefined;
@@ -55,13 +56,12 @@ const Preview: React.FC<PreviewProps> = ({ className }) => {
     },
   );
 
-
-  const handleCustomRedirect = (e) => {
-    const target = e.target as HTMLElement;
-    if (target.closest('a[href]')) {
-      e.preventDefault(); 
+  const handleCustomRedirect = useEventCallback((e: React.MouseEvent<HTMLElement>) => {
+    if (e.target.tagName === 'A') {
+      e.preventDefault();
     }
-  };
+  })
+
 
   return (
     <div ref={exampleRef}>
