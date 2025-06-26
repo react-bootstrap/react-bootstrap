449c75d14e0acd96eb0582afd4b834996e203ff6
kyletsang <6854874+kyletsang@users.noreply.github.com>
2025-06-25 23:14:40 -0700

    feat(AccordionHeader): add disabled prop

--- test/AccordionHeaderSpec.tsx
+++ test/AccordionHeaderSpec.tsx
@@ -1,5 +1,6 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { AccordionHeader } from '../src';
 
 describe('<AccordionHeader>', () => {
@@ -9,5 +10,15 @@ describe('<AccordionHeader>', () => {
     expect(screen.getByRole('button').getAttribute('aria-controls')).toEqual(
       'test',
     );
+  });
+
+  it('should disable the header button when disabled prop is true', async () => {
+    const user = userEvent.setup();
+    const onClickFn = vi.fn();
+    render(<AccordionHeader disabled onClick={onClickFn} />);
+
+    await user.click(screen.getByRole('button'));
+
+    expect(onClickFn).not.toHaveBeenCalled();
   });
 });
