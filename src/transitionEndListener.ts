dcfe5dab70d12da3473d1a3b26fbfa2460fde65c
Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
2025-08-21 22:35:23 -0700

    fix: fix exit animations for various transitions (#6954)

--- src/transitionEndListener.ts
+++ src/transitionEndListener.ts
@@ -1,10 +1,10 @@
 import transitionEnd from 'dom-helpers/transitionEnd';
 
-function parseDuration(
+export function parseDuration(
   node: HTMLElement,
   property: 'transition-duration' | 'transition-delay',
 ) {
-  const str = node.style.getPropertyValue(property);
+  const str = getComputedStyle(node).getPropertyValue(property);
   const mult = str.indexOf('ms') === -1 ? 1000 : 1;
   return parseFloat(str) * mult;
 }
